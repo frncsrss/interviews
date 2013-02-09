c802b7170e934bc08abe0430c3779f86c0e79985
François Rousseau <frncs.rss@gmail.com>
2013-02-09 19:04:25 +0100

    Substring problem updated.
    Another implementation of the Knutt-Morris-Patt algorithm much easier
    to follow. Explanatory javadoc with example introduced for that method.

--- src/core/interviews/strings/Substring.java
+++ src/core/interviews/strings/Substring.java
@@ -1,12 +1,12 @@
 package interviews.strings;
 
 /**
- * Check if a string is a substring of another.
+ * Returns the first index where a pattern p appears in a string s.
  * @author Francois Rousseau
  */
 public class Substring {
   /**
-   * Check if a string is a substring of another in O(n*m).
+   * Returns the first index where a pattern p appears in a string s in O(n*m).
    * @return the index of first match if it exists.
    */
   public static int strstrBruteForce(String s, String p) {
@@ -14,7 +14,7 @@ public class Substring {
   }
 
   /**
-   * Check if a string is a substring of another in O(n*m).
+   * Returns the first index where a pattern p appears in a string s in O(n*m).
    * @return the index of first match if it exists.
    */
   public static int strstrBruteForce(char[] s, char[] p) {
@@ -31,8 +31,8 @@ public class Substring {
   }
 
   /**
-   * Check if a string is a substring of another in O(n + m).
-   * Rely on the Knutt-Morris-Pratt algorithm.
+   * Returns the first index where a pattern p appears in a string s in O(n + m).
+   * Rely on the Knutt-Morris-Pratt algorithm. Explained below.
    * @return the index of first match if it exists.
    */
   public static int strstrKMP(String s, String p) {
@@ -40,8 +40,8 @@ public class Substring {
   }
 
   /**
-   * Check if a string is a substring of another in O(n + m).
-   * Rely on the Knutt-Morris-Pratt algorithm.
+   * Returns the first index where a pattern p appears in a string s in O(n + m).
+   * Rely on the Knutt-Morris-Pratt algorithm. Explained below.
    * @return the index of first match if it exists.
    */
   public static int strstrKMP(char[] s, char[] p) {
@@ -50,13 +50,13 @@ public class Substring {
     int j = 0;  // position in pattern
 
     while(i < s.length) {  // iterate over the entire text - O(n)
-      while (j >= 0 && p[j] != s[i]) {
-        j = t[j];
+      while(j >= 0 && p[j] != s[i]) {
+        j = t[j];  // instead of shifting the window by 1, we shift the window by t[j] + 1
       }
       // at this point either j==0 (back to the beginning) or p[j] == s[i]
       i++;
       j++;
-      if (j == p.length) {  // we got what we wanted
+      if(j == p.length) {  // we got what we wanted
         return i - p.length;
       }
     }
@@ -64,11 +64,14 @@ public class Substring {
   }
 
   /**
-   * Returns for each position the longest prefix matching a suffix in the
-   * substring before that position.
-   * @return the prefix table.
+   * Computes the length of the longest prefix matching a suffix for every substring of p
+   * and returns a prefix table t as follows:
+   * t[i] == length of the longest prefix matching a suffix in p[0..i-1]
+   * with t[0] == -1 (see above).
+   * 
+   * @return the index of first match if it exists.
    */
-  public static int[] getPrefixTable(char[] p) {
+  protected static int[] getPrefixTable(char[] p) {
     final int[] t = new int[p.length + 1];
     int i = 0;  // position in pattern
     int j = -1; // length of the current prefix
@@ -84,6 +87,91 @@ public class Substring {
       i++;
       j++;
       t[i] = j;
+    }
+    return t;
+  }
+
+  /**
+   * Returns the first index where a pattern p appears in a string s in O(n + m).
+   * Rely on the Knutt-Morris-Pratt algorithm. Explained below.
+   * @return the index of first match if it exists.
+   */
+  public static int strstrKMP2(String s, String p) {
+    return strstrKMP2(s.toCharArray(), p.toCharArray());
+  }
+
+  /**
+   * Returns the first index where a pattern p appears in a string s in O(n + m).
+   * 
+   * Rely on the Knutt-Morris-Pratt algorithm. Explained below.
+   * For example, consider:
+   * i   0123456789
+   * s = abcdab abc (i=0)
+   * p = abcdabd    (j=0)
+   * j   0123456
+   * 
+   * The first mismatch occurs at j==6.
+   * Considering p[0..5], the length of the longest prefix matching a suffix is 2 ("ab").
+   * Therefore, you should move i from 0 to 4 (6-2) but start j at 2 since we already know that the
+   * first 2 characters are matching (this is why the complexity is O(n) and not O(n*m)).
+   * i   0123456789
+   * s = abcdab abc  (i=4)
+   * p =     abcdabd (j=2)
+   * j       0123456
+   * 
+   * Computing the length of the longest prefix matching a suffix for every substring of p doesn't
+   * depend on s and is therefore pre-computed and stored in a prefix table t as follows:
+   * t[i] == length of the longest prefix matching a suffix in p[0..i-1]
+   * with t[0] == -1 to increment i if mismatch at first character (we slide by j - t[j]).
+   * 
+   * @return the index of first match if it exists.
+   */
+  public static int strstrKMP2(char[] s, char[] p) {
+    final int[] t = getPrefixTable2(p);
+    int i = 0;  // position in text
+    int j = 0;  // position in pattern
+
+    while(i+j < s.length) {  // iterate over the entire text - O(n)
+      if(s[i+j] == p[j]) {
+        if (j == p.length - 1) {  // reached the end of the pattern - we got what we wanted
+          return i;
+        }
+        j++;  // otherwise move along to the next character
+      } else {
+        i += j - t[j];  // sliding in s by j - t[j]
+        if(j > 0) {
+          j = t[j];  // starting at t[j] in p - prevents the matching of already seen characters
+        }  // for j==0, t[j] = -1 so we need j to remains equal to 0
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Computes the length of the longest prefix matching a suffix for every substring of p
+   * and returns a prefix table t as follows:
+   * t[i] == length of the longest prefix matching a suffix in p[0..i-1]
+   * with t[0] == -1 (see above).
+   * 
+   * @return the index of first match if it exists.
+   */
+  protected static int[] getPrefixTable2(char[] p) {
+    final int[] t = new int[p.length];
+    t[0] = -1;  // special value for the KMP algorithm
+    t[1] = 0;  // we consider only proper prefix/suffix so p[0] doesn't contain any
+
+    int i = 2;  // position in pattern
+    int len = 0; // length of the current prefix
+
+    while(i < p.length) {  // iterate over the entire pattern - O(m)
+      if(p[i-1] == p[len]) {  // we have one more match
+        len++;
+        t[i++] = len;  // set the length and move along to the next character
+      } else if(len > 0) {  // mismatch but still smaller prefixes to try
+        len = t[len];  // rollback to the second longest prefix for the prefix and try again
+      } else {  // mismatch for the first character
+        t[i++] = 0;  // set the length and move along to the next character
+      }
     }
     return t;
   }
--- src/test/interviews/strings/SubstringTest.java
+++ src/test/interviews/strings/SubstringTest.java
@@ -4,8 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static interviews.strings.Substring.getPrefixTable;
+import static interviews.strings.Substring.getPrefixTable2;
 import static interviews.strings.Substring.strstrBruteForce;
 import static interviews.strings.Substring.strstrKMP;
+import static interviews.strings.Substring.strstrKMP2;
 
 /**
  * Test class.
@@ -13,7 +15,7 @@ import static interviews.strings.Substring.strstrKMP;
  */
 public class SubstringTest {
   @Test
-  public void test_strstr() {
+  public void test_strstrBruteForce() {
     Assert.assertEquals(4,  strstrBruteForce("ababaaab", "aaa"));
     Assert.assertEquals(5,  strstrBruteForce("ababaaab", "aab"));
     Assert.assertEquals(-1, strstrBruteForce("ababaaab", "aac"));
@@ -23,10 +25,11 @@ public class SubstringTest {
     Assert.assertEquals(-1, strstrBruteForce("abacadabrabracabracadabrabrabracad", "bcara"));
     Assert.assertEquals(23, strstrBruteForce("abacadabrabracabracadabrabrabracad", "rabrabracad"));
     Assert.assertEquals(0,  strstrBruteForce("abacadabrabracabracadabrabrabracad", "abacad"));
+    Assert.assertEquals(13, strstrBruteForce("abra abracad abracadabra", "abracadabra"));
   }
 
   @Test
-  public void test_search() {
+  public void test_strstrKMP() {
     Assert.assertEquals(4,  strstrKMP("ababaaab", "aaa"));
     Assert.assertEquals(5,  strstrKMP("ababaaab", "aab"));
     Assert.assertEquals(-1, strstrKMP("ababaaab", "aac"));
@@ -36,11 +39,40 @@ public class SubstringTest {
     Assert.assertEquals(-1, strstrKMP("abacadabrabracabracadabrabrabracad", "bcara"));
     Assert.assertEquals(23, strstrKMP("abacadabrabracabracadabrabrabracad", "rabrabracad"));
     Assert.assertEquals(0,  strstrKMP("abacadabrabracabracadabrabrabracad", "abacad"));
+    Assert.assertEquals(13, strstrKMP("abra abracad abracadabra", "abracadabra"));
   }
 
   @Test
-  public void test_prefix() {
+  public void test_prefixTable() {
     Assert.assertArrayEquals(
         new int[]{-1, 0, 0, 0, 1, 0, 1, 2, 3, 4, 2, 3, 4}, getPrefixTable("atcacatcatca".toCharArray()));
+    Assert.assertArrayEquals(
+        new int[]{-1, 0, 0, 1, 2, 3, 4, 0, 1}, getPrefixTable("abababca".toCharArray()));
+    Assert.assertArrayEquals(
+        new int[]{-1, 0, 0, 0, 1, 0, 1, 0, 1, 2, 3, 4}, getPrefixTable("abracadabra".toCharArray()));
+  }
+
+  @Test
+  public void test_strstrKMP2() {
+    Assert.assertEquals(4,  strstrKMP2("ababaaab", "aaa"));
+    Assert.assertEquals(5,  strstrKMP2("ababaaab", "aab"));
+    Assert.assertEquals(-1, strstrKMP2("ababaaab", "aac"));
+    Assert.assertEquals(-1, strstrKMP2("ababaaab", "aaaa"));
+    Assert.assertEquals(14, strstrKMP2("abacadabrabracabracadabrabrabracad", "abracadabra"));
+    Assert.assertEquals(8,  strstrKMP2("abacadabrabracabracadabrabrabracad", "rab"));
+    Assert.assertEquals(-1, strstrKMP2("abacadabrabracabracadabrabrabracad", "bcara"));
+    Assert.assertEquals(23, strstrKMP2("abacadabrabracabracadabrabrabracad", "rabrabracad"));
+    Assert.assertEquals(0,  strstrKMP2("abacadabrabracabracadabrabrabracad", "abacad"));
+    Assert.assertEquals(13, strstrKMP2("abra abracad abracadabra", "abracadabra"));
+  }
+
+  @Test
+  public void test_prefixTable2() {
+    Assert.assertArrayEquals(
+        new int[]{-1, 0, 0, 0, 1, 0, 1, 2, 3, 4, 2, 3}, getPrefixTable2("atcacatcatca".toCharArray()));
+    Assert.assertArrayEquals(
+        new int[]{-1, 0, 0, 1, 2, 3, 4, 0}, getPrefixTable2("abababca".toCharArray()));
+    Assert.assertArrayEquals(
+        new int[]{-1, 0, 0, 0, 1, 0, 1, 0, 1, 2, 3}, getPrefixTable2("abracadabra".toCharArray()));
   }
 }
