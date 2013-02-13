32bfb5bceabab159957a460f4bc193d66d2cc69e
François Rousseau <frncs.rss@gmail.com>
2013-02-13 15:44:17 +0100

    shuffle

--- src/core/interviews/arrays/Shuffle.java
+++ src/core/interviews/arrays/Shuffle.java
@@ -10,7 +10,7 @@ public class Shuffle {
   public static void f(List<Integer> list) {
     final int n = list.size();
     for(int i = 0; i < n; i++) {
-      final int j = (int) (Math.random() * (n-i))+i;
+      final int j = (int) (Math.random() * (n-i)) + i;  // we pick a random index between i and n-1
       Swap.f(list, i, j);
     }
   }
@@ -18,7 +18,7 @@ public class Shuffle {
   public static void f(int[] arr) {
     final int n = arr.length;
     for(int i = 0; i < n; i++) {
-      final int j = (int) (Math.random() * (n-i)) + i;
+      final int j = (int) (Math.random() * (n-i)) + i;  // we pick a random index between i and n-1
       Swap.f(arr, i, j);
     }
   }
