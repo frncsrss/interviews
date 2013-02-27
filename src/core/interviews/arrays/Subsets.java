1967a85df19cbf7181bb4d31d4b65af53a51cc3a
François Rousseau <frncs.rss@gmail.com>
2013-02-27 15:38:52 +0100

    overflow exception for list of subsets

--- src/core/interviews/arrays/Subsets.java
+++ src/core/interviews/arrays/Subsets.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Returns all the 2^n subsets of a list of size n.
+ * Returns all the 2^n subsets of a list of size n <= 31.
  * @author Francois Rousseau
  */
 public class Subsets {
   public static <E> List<List<E>> f(List<E> set) {
+    if(set.size() > 31) {
+      throw new UnsupportedOperationException();
+    }
     final int nb_subsets = 1 << set.size();  // 2^n
     List<List<E>> subsets = new ArrayList<List<E>>(nb_subsets);
-    for(int i=0; i<nb_subsets; i++) {
+    for(int i = 0; i < nb_subsets; i++) {
       subsets.add(subset(set, i));
     }
     return subsets;
   }
 
   private static <E> List<E> subset(List<E> set, int x) {
-    List<E> subset = new ArrayList<E>();
-    int index = 0;
-    for(int k=x; k>0; k >>= 1) {  // up to 32 iterations
-      if((k&1) == 1) {
+    final List<E> subset = new ArrayList<E>();  // up to 32 elements
+    for(int k = x, index = 0; k > 0; k >>= 1) {  // up to 32 iterations
+      if((k&1) == 1) {  // we add the element of the set if its bit is set to 1
         subset.add(set.get(index));
       }
       index++;
--- src/test/interviews/arrays/SubsetsTest.java
+++ src/test/interviews/arrays/SubsetsTest.java
@@ -16,14 +16,36 @@ public class SubsetsTest {
   @Test
   public void test() {
     Assert.assertEquals(
-        2, f(new ArrayList<Integer>(Arrays.asList(0))).size());
+        2,    f(new ArrayList<Integer>(Arrays.asList(0))).size());
     Assert.assertEquals(
-        4, f(new ArrayList<Integer>(Arrays.asList(0, 1))).size());
+        4,    f(new ArrayList<Integer>(Arrays.asList(0, 1))).size());
     Assert.assertEquals(
-        8, f(new ArrayList<Integer>(Arrays.asList(0, 1, 2))).size());
+        8,    f(new ArrayList<Integer>(Arrays.asList(0, 1, 2))).size());
     Assert.assertEquals(
-        16, f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3))).size());
+        16,   f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3))).size());
     Assert.assertEquals(
-        32, f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4))).size());
+        32,   f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4))).size());
+    Assert.assertEquals(
+        64,   f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5))).size());
+    Assert.assertEquals(
+        128,  f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6))).size());
+    Assert.assertEquals(
+        256,  f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7))).size());
+    Assert.assertEquals(
+        512,  f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8))).size());
+    Assert.assertEquals(
+        1024, f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))).size());
+    Assert.assertEquals(
+        2048, f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))).size());
+    Assert.assertEquals(
+        4096, f(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))).size());
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void test_exception() {
+    f(new ArrayList<Integer>(
+        Arrays.asList(
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26, 27, 28, 29, 30, 31))).size();
   }
 }
