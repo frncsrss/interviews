d5fe794b1146932e4f867aa39933a34155a3ea64
François Rousseau <frncs.rss@gmail.com>
2013-03-04 18:18:14 +0100

    refactored BSTTest class

--- src/test/interviews/trees/BSTTest.java
+++ src/test/interviews/trees/BSTTest.java
@@ -14,6 +14,22 @@ import org.junit.Test;
  */
 public class BSTTest {
 
+  private static BST<Integer> getBSTOfIntegers() {
+    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(3);
+    bst.insert(8);
+    bst.insert(2);
+    bst.insert(4);
+    bst.insert(6);
+    bst.insert(9);
+    bst.insert(14);
+    bst.insert(11);
+    bst.insert(16);
+    return bst;
+  }
+
   @Test(expected = NullPointerException.class)
   public void test_ceiling_exception() {
     BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
@@ -58,18 +74,7 @@ public class BSTTest {
 
   @Test
   public void test_ceiling() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(null, bst.ceiling(17));
     Assert.assertEquals(new Integer(8), bst.ceiling(7));
     Assert.assertEquals(new Integer(8), bst.ceiling(8));
@@ -78,18 +83,7 @@ public class BSTTest {
 
   @Test
   public void test_count() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(11, bst.count(1, 17));
     Assert.assertEquals(11, bst.count(2, 16));
     Assert.assertEquals(4, bst.count(3, 7));
@@ -97,35 +91,13 @@ public class BSTTest {
 
   @Test
   public void test_insert() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(11, bst.size());
   }
 
   @Test
   public void test_floor() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(null, bst.floor(1));
     Assert.assertEquals(new Integer(6), bst.floor(7));
     Assert.assertEquals(new Integer(6), bst.floor(6));
@@ -134,52 +106,19 @@ public class BSTTest {
 
   @Test
   public void test_max() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(new Integer(16), bst.max());
   }
 
   @Test
   public void test_min() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(new Integer(2), bst.min());
   }
 
   @Test
   public void test_rank() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(0, bst.rank(1));
     Assert.assertEquals(5, bst.rank(7));
     Assert.assertEquals(4, bst.rank(6));
@@ -188,18 +127,7 @@ public class BSTTest {
 
   @Test
   public void test_delete() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals("10 5 14 3 8 11 16 2 4 6 9", bst.toString());
     bst.delete(5);
     Assert.assertEquals("10 6 14 3 8 11 16 2 4 9", bst.toString());
@@ -222,73 +150,27 @@ public class BSTTest {
 
   @Test
   public void test_size() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    Assert.assertEquals(0, bst.size());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(11, bst.size());
   }
 
   @Test
   public void test_traversalInOrderRecursive() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(Arrays.asList(2, 3, 4, 5, 6, 8, 9, 10, 11, 14, 16), bst.traversalInOrderRecursive());
     Assert.assertEquals(Arrays.asList(2, 3, 4, 5, 6, 8, 9, 10, 11, 14, 16), bst.traversalInOrderRecursive());
   }
 
   @Test
   public void test_traversalInOrderNonRecursive() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals(Arrays.asList(2, 3, 4, 5, 6, 8, 9, 10, 11, 14, 16), bst.traversalInOrderNonRecursive());
     Assert.assertEquals(Arrays.asList(2, 3, 4, 5, 6, 8, 9, 10, 11, 14, 16), bst.traversalInOrderNonRecursive());
   }
 
   @Test
   public void test_toString() {
-    BST<Integer> bst = new BST<Integer>(Sorts.getComparatorOfIntegers());
-    Assert.assertEquals("", bst.toString());
-    bst.insert(10);
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(8);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.insert(9);
-    bst.insert(14);
-    bst.insert(11);
-    bst.insert(16);
+    BST<Integer> bst = getBSTOfIntegers();
     Assert.assertEquals("10 5 14 3 8 11 16 2 4 6 9", bst.toString());
   }
 }
