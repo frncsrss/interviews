6f1bfd6097a6b037bada6a3ff1e1ca94ed2a0540
François Rousseau <frncs.rss@gmail.com>
2013-03-14 21:07:32 +0100

    updated Binomial class

--- src/core/interviews/numbers/Binomial.java
+++ src/core/interviews/numbers/Binomial.java
@@ -1,25 +1,19 @@
 package interviews.numbers;
 
 /**
- * n choose k in O(k).
+ * n choose k in O(n^2), O(nk) and O(k).
  * @author Francois Rousseau
  */
 public class Binomial {
-
-  public static long f(int n, int k) {
-    return binomial_nk(n, k);
-  }
-
-  /*
+  /**
    * Returns (k..n) in O(n^2) time, O(n) space.
    */
-  public static long binomial_n2(int n, int k) {
-    // we use the symmetry property.
-    if (k > n-k) {
-      k = n-k;
-    }
-    if(k < 0) {
+  public static long f_n2(int n, int k) {
+    if(k < 0 || k > n) {
       return 0;
+    }
+    if (k > n-k) {  // we use the symmetry property
+      k = n-k;
     }
     if(k == 0) {
       return 1;
@@ -27,35 +21,32 @@ public class Binomial {
     if(k == 1) {
       return n;
     }
-    long[] b_n = new long[n+1];
+    long[] b_n = new long[n + 1];  // hold the current line of Pascal's triangle
     b_n[0] = 1;  // (0..n) = 1
     b_n[1] = 1;  // (1..1) = 1
     for(int i = 2 ; i < n; i++) {
       b_n[i] = 1;
       b_n[i-1] = i;
-      // we go backwards so we can overwrite without problems.
-      for(int j=i-2;j>1;j--) {
+      for(int j=i-2;j>1;j--) {  // we go backwards so we can overwrite without problems
         b_n[j] += b_n[j-1];  // (k..n) = (k-1..n-1) + (k..n-1)
       }
       b_n[1] = i;
     }    
-    // for the last line, we can stop once we have b_n[n-k].
-    for(int j=n-1;j>=n-k;j--) {
-      b_n[j] += b_n[j-1];
+    for(int j = n - 1; j >= n - k; j--) {  // for the last line, we can stop once we have b_n[n-k]
+      b_n[j] += b_n[j - 1];
     }
     return b_n[n-k];
   }
 
-  /*
+  /**
    * Returns (k..n) in O(nk) time, O(n) space.
    */
-  public static long binomial_nk(int n, int k) {
-    // we use the symmetry property.
-    if (k > n-k) {
-      k = n-k;
-    }
-    if(k < 0) {
+  public static long f_nk(int n, int k) {
+    if(k < 0 || k > n) {
       return 0;
+    }
+    if (k > n-k) {  // we use the symmetry property
+      k = n-k;
     }
     if(k == 0) {
       return 1;
@@ -63,7 +54,7 @@ public class Binomial {
     if(k == 1) {
       return n;
     }
-    long[] b_n = new long[n+1];
+    long[] b_n = new long[n+1];  // hold the current line of Pascal's triangle
     b_n[0] = 1;  // (0..n) = 1
     b_n[1] = 1;  // (1..1) = 1
     for(int i = 2; i <= n; i++) {
@@ -72,7 +63,7 @@ public class Binomial {
       // we go backwards so we can overwrite without problems.
       // we also know we don't need more than k values.
       // hence the O(nk) complexity.
-      for(int j=i-2, count=0;j>1 && count<k;j--, count++) {
+      for(int j = i - 2, count = 0; j > 1 && count < k; j--, count++) {
         b_n[j] += b_n[j-1];  // (k..n) = (k-1..n-1) + (k..n-1)
       }
       b_n[1] = i;
@@ -80,15 +71,14 @@ public class Binomial {
     return b_n[n-k];
   }
 
-  /*
+  /**
    * Returns (k..n) in O(k) time, O(1) space.
    */
-  public static long binomial_k(int n, int k) {
+  public static long f_k(int n, int k) {
     if(k < 0 || k > n) {
       return 0;
     }
-    // we use the symmetry property.
-    if (k > n-k) {
+    if (k > n-k) {  // we use the symmetry property
       k = n-k;
     }
     if(k == 0) {
--- src/test/interviews/numbers/BinomialTest.java
+++ src/test/interviews/numbers/BinomialTest.java
@@ -9,21 +9,59 @@ import org.junit.Test;
  */
 public class BinomialTest {
   @Test
-  public void test() {
-    Assert.assertEquals(1, Binomial.f(8, 0));
-    Assert.assertEquals(8, Binomial.f(8, 1));
-    Assert.assertEquals(28, Binomial.f(8, 2));
-    Assert.assertEquals(56, Binomial.f(8, 3));
-    Assert.assertEquals(70, Binomial.f(8, 4));
-    Assert.assertEquals(56, Binomial.f(8, 5));
-    Assert.assertEquals(28, Binomial.f(8, 6));
-    Assert.assertEquals(8, Binomial.f(8, 7));
-    Assert.assertEquals(1, Binomial.f(8, 8));
-    Assert.assertEquals(0, Binomial.f(8, 10));
-    Assert.assertEquals(364, Binomial.f(14, 3));
-    Assert.assertEquals(161700, Binomial.f(100, 3));
-    Assert.assertEquals(86493225, Binomial.f(30, 12));
-    Assert.assertEquals(121399651100L, Binomial.f(50, 12));
-    Assert.assertEquals(1399358844975L, Binomial.f(60, 12));
+  public void test_f_n2() {
+    Assert.assertEquals(1, Binomial.f_n2(8, 0));
+    Assert.assertEquals(8, Binomial.f_n2(8, 1));
+    Assert.assertEquals(28, Binomial.f_n2(8, 2));
+    Assert.assertEquals(56, Binomial.f_n2(8, 3));
+    Assert.assertEquals(70, Binomial.f_n2(8, 4));
+    Assert.assertEquals(56, Binomial.f_n2(8, 5));
+    Assert.assertEquals(28, Binomial.f_n2(8, 6));
+    Assert.assertEquals(8, Binomial.f_n2(8, 7));
+    Assert.assertEquals(1, Binomial.f_n2(8, 8));
+    Assert.assertEquals(0, Binomial.f_n2(8, 10));
+    Assert.assertEquals(364, Binomial.f_n2(14, 3));
+    Assert.assertEquals(161700, Binomial.f_n2(100, 3));
+    Assert.assertEquals(86493225, Binomial.f_n2(30, 12));
+    Assert.assertEquals(121399651100L, Binomial.f_n2(50, 12));
+    Assert.assertEquals(1399358844975L, Binomial.f_n2(60, 12));
+  }
+
+  @Test
+  public void test_f_nk() {
+    Assert.assertEquals(1, Binomial.f_nk(8, 0));
+    Assert.assertEquals(8, Binomial.f_nk(8, 1));
+    Assert.assertEquals(28, Binomial.f_nk(8, 2));
+    Assert.assertEquals(56, Binomial.f_nk(8, 3));
+    Assert.assertEquals(70, Binomial.f_nk(8, 4));
+    Assert.assertEquals(56, Binomial.f_nk(8, 5));
+    Assert.assertEquals(28, Binomial.f_nk(8, 6));
+    Assert.assertEquals(8, Binomial.f_nk(8, 7));
+    Assert.assertEquals(1, Binomial.f_nk(8, 8));
+    Assert.assertEquals(0, Binomial.f_nk(8, 10));
+    Assert.assertEquals(364, Binomial.f_nk(14, 3));
+    Assert.assertEquals(161700, Binomial.f_nk(100, 3));
+    Assert.assertEquals(86493225, Binomial.f_nk(30, 12));
+    Assert.assertEquals(121399651100L, Binomial.f_nk(50, 12));
+    Assert.assertEquals(1399358844975L, Binomial.f_nk(60, 12));
+  }
+
+  @Test
+  public void test_f_k() {
+    Assert.assertEquals(1, Binomial.f_k(8, 0));
+    Assert.assertEquals(8, Binomial.f_k(8, 1));
+    Assert.assertEquals(28, Binomial.f_k(8, 2));
+    Assert.assertEquals(56, Binomial.f_k(8, 3));
+    Assert.assertEquals(70, Binomial.f_k(8, 4));
+    Assert.assertEquals(56, Binomial.f_k(8, 5));
+    Assert.assertEquals(28, Binomial.f_k(8, 6));
+    Assert.assertEquals(8, Binomial.f_k(8, 7));
+    Assert.assertEquals(1, Binomial.f_k(8, 8));
+    Assert.assertEquals(0, Binomial.f_k(8, 10));
+    Assert.assertEquals(364, Binomial.f_k(14, 3));
+    Assert.assertEquals(161700, Binomial.f_k(100, 3));
+    Assert.assertEquals(86493225, Binomial.f_k(30, 12));
+    Assert.assertEquals(121399651100L, Binomial.f_k(50, 12));
+    Assert.assertEquals(1399358844975L, Binomial.f_k(60, 12));
   }
 }
