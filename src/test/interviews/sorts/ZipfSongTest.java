001a6871ce70c8630f85ea51ded6cdf3beadae42
François Rousseau <frncs.rss@gmail.com>
2013-03-18 21:27:43 +0100

    added one unittest for maximum length file

--- src/test/interviews/sorts/ZipfSongTest.java
+++ src/test/interviews/sorts/ZipfSongTest.java
@@ -3,6 +3,7 @@ package interviews.sorts;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Random;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,7 +20,8 @@ public class ZipfSongTest {
         + "30 one\n30 two\n15 three\n25 four\n";
     System.setIn(new ByteArrayInputStream(input.getBytes()));
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(baos);    System.setOut(ps);
+    PrintStream ps = new PrintStream(baos);
+    System.setOut(ps);
     ZipfSong.main(null);
     Assert.assertEquals("four\ntwo\n", baos.toString());
   }
@@ -33,7 +35,8 @@ public class ZipfSongTest {
         + "12139 starshine\n11272 slow_country\n10521 m1_a1\n";
     System.setIn(new ByteArrayInputStream(input.getBytes()));
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(baos);    System.setOut(ps);
+    PrintStream ps = new PrintStream(baos);
+    System.setOut(ps);
     ZipfSong.main(null);
     Assert.assertEquals("19_2000\nclint_eastwood\ntomorrow_comes_today\n", baos.toString());
   }
@@ -44,9 +47,29 @@ public class ZipfSongTest {
         + "1000000000000 one\n30 two\n15 three\n25 four\n";
     System.setIn(new ByteArrayInputStream(input.getBytes()));
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(baos);    System.setOut(ps);
+    PrintStream ps = new PrintStream(baos);
+    System.setOut(ps);
     ZipfSong.main(null);
     Assert.assertEquals("one\nfour\n", baos.toString());
+  }
+
+  @Test
+  public void test_50000()  {
+    Random random = new Random(1234);
+    StringBuilder builder = new StringBuilder();
+    builder.append("50000 10\n");
+    for(int i = 0; i < 50000; i++) {
+      builder.append(((int) random.nextInt(Integer.MAX_VALUE)/(i + 1)) + " song_" + i + "\n");
+    }
+    System.setIn(new ByteArrayInputStream(builder.toString().getBytes()));
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    System.setOut(ps);
+    ZipfSong.main(null);
+    Assert.assertEquals(
+        "song_33449\nsong_16408\nsong_35336\nsong_23404\nsong_43725\nsong_6953\nsong_49053\n"
+        + "song_21289\nsong_43211\nsong_36468\n",
+        baos.toString());
   }
 
 }
