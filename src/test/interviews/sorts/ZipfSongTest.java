22933b1a0660e0f3f17d1ec78b4f92a6e97605a8
François Rousseau <frncs.rss@gmail.com>
2013-03-18 21:16:43 +0100

    added unittest for ZipfSong

---
+++ src/test/interviews/sorts/ZipfSongTest.java
@@ -0,0 +1,52 @@
+package interviews.sorts;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test class.
+ * @author Francois Rousseau
+ */
+public class ZipfSongTest {
+
+  @Test
+  public void test_input1()  {
+    String input = "4 2\n"
+        + "30 one\n30 two\n15 three\n25 four\n";
+    System.setIn(new ByteArrayInputStream(input.getBytes()));
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);    System.setOut(ps);
+    ZipfSong.main(null);
+    Assert.assertEquals("four\ntwo\n", baos.toString());
+  }
+
+  @Test
+  public void test_input2()  {
+    String input = "15 3\n"
+        + "197812 re_hash\n78906 5_4\n189518 tomorrow_comes_today\n39453 new_genious\n"
+        + "210492 clint_eastwood\n26302 man_research\n22544 punk\n19727 sound_check\n"
+        + "17535 double_bass\n18782 rock_the_house\n198189 19_2000\n13151 latin_simone\n"
+        + "12139 starshine\n11272 slow_country\n10521 m1_a1\n";
+    System.setIn(new ByteArrayInputStream(input.getBytes()));
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);    System.setOut(ps);
+    ZipfSong.main(null);
+    Assert.assertEquals("19_2000\nclint_eastwood\ntomorrow_comes_today\n", baos.toString());
+  }
+
+  @Test
+  public void test_long()  {
+    String input = "4 2\n"
+        + "1000000000000 one\n30 two\n15 three\n25 four\n";
+    System.setIn(new ByteArrayInputStream(input.getBytes()));
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);    System.setOut(ps);
+    ZipfSong.main(null);
+    Assert.assertEquals("one\nfour\n", baos.toString());
+  }
+
+}
