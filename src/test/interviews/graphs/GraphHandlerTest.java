9d555e2d3f163b192da7c218c2c78bb197b3abf1
Francois Rousseau <frncs.rss@gmail.com>
2013-03-28 10:46:20 +0100

    added test for directed bfs

--- src/test/interviews/graphs/GraphHandlerTest.java
+++ src/test/interviews/graphs/GraphHandlerTest.java
@@ -24,15 +24,30 @@ public class GraphHandlerTest extends GraphTest {
 
   @Test
   public void test_bfs() {
+    setUpDirected();
+    gh.bfs(0);
+    Assert.assertEquals(null, gh.parent(0));
+    Assert.assertEquals(new Integer(0), gh.parent(1));
+    Assert.assertEquals(new Integer(4), gh.parent(2));
+    Assert.assertEquals(new Integer(4), gh.parent(3));
+    Assert.assertEquals(new Integer(5), gh.parent(4));
+    Assert.assertEquals(new Integer(0), gh.parent(5));
+    Assert.assertEquals(null, gh.parent(6));
+    Assert.assertEquals(null, gh.parent(7));
+    Assert.assertEquals(null, gh.parent(8));
+    Assert.assertEquals(null, gh.parent(9));
+    Assert.assertEquals(null, gh.parent(10));
+    Assert.assertEquals(null, gh.parent(11));
+    Assert.assertEquals(null, gh.parent(12));
+    Assert.assertEquals("[3, 4, 5, 0]", gh.pathTo(3).toString());
+
     setUpUndirected();
     gh.bfs(0);
     Assert.assertEquals(null, gh.parent(0));
     Assert.assertEquals(new Integer(0), gh.parent(1));
     Assert.assertEquals(new Integer(0), gh.parent(2));
     Assert.assertEquals(new Integer(5), gh.parent(3));
-    Assert.assertEquals("[3, 5, 0]", gh.pathTo(3).toString());
     Assert.assertEquals(new Integer(6), gh.parent(4));
-    Assert.assertEquals("[4, 6, 0]", gh.pathTo(4).toString());
     Assert.assertEquals(new Integer(0), gh.parent(5));
     Assert.assertEquals(new Integer(0), gh.parent(6));
     Assert.assertEquals(null, gh.parent(7));
@@ -41,6 +56,8 @@ public class GraphHandlerTest extends GraphTest {
     Assert.assertEquals(null, gh.parent(10));
     Assert.assertEquals(null, gh.parent(11));
     Assert.assertEquals(null, gh.parent(12));
+    Assert.assertEquals("[3, 5, 0]", gh.pathTo(3).toString());
+    Assert.assertEquals("[4, 6, 0]", gh.pathTo(4).toString());
   }
 
   @Test
@@ -84,7 +101,6 @@ public class GraphHandlerTest extends GraphTest {
     Assert.assertEquals(null, gh.parent(0));
     Assert.assertEquals(new Integer(0), gh.parent(1));
     Assert.assertEquals(new Integer(3), gh.parent(2));
-    Assert.assertEquals("[2, 3, 4, 5, 0]", gh.pathTo(2).toString());
     Assert.assertEquals(new Integer(4), gh.parent(3));
     Assert.assertEquals(new Integer(5), gh.parent(4));
     Assert.assertEquals(new Integer(0), gh.parent(5));
@@ -95,6 +111,7 @@ public class GraphHandlerTest extends GraphTest {
     Assert.assertEquals(null, gh.parent(10));
     Assert.assertEquals(null, gh.parent(11));
     Assert.assertEquals(null, gh.parent(12));
+    Assert.assertEquals("[2, 3, 4, 5, 0]", gh.pathTo(2).toString());
 
     setUpUndirected();
     gh.dfs(0);
@@ -102,17 +119,17 @@ public class GraphHandlerTest extends GraphTest {
     Assert.assertEquals(new Integer(0), gh.parent(1));
     Assert.assertEquals(new Integer(0), gh.parent(2));
     Assert.assertEquals(new Integer(5), gh.parent(3));
-    Assert.assertEquals("[3, 5, 0]", gh.pathTo(3).toString());
     Assert.assertEquals(new Integer(3), gh.parent(4));
-    Assert.assertEquals("[4, 3, 5, 0]", gh.pathTo(4).toString());
     Assert.assertEquals(new Integer(0), gh.parent(5));
     Assert.assertEquals(new Integer(4), gh.parent(6));
-    Assert.assertEquals("[6, 4, 3, 5, 0]", gh.pathTo(6).toString());
     Assert.assertEquals(null, gh.parent(7));
     Assert.assertEquals(null, gh.parent(8));
     Assert.assertEquals(null, gh.parent(9));
     Assert.assertEquals(null, gh.parent(10));
     Assert.assertEquals(null, gh.parent(11));
     Assert.assertEquals(null, gh.parent(12));
+    Assert.assertEquals("[3, 5, 0]", gh.pathTo(3).toString());
+    Assert.assertEquals("[4, 3, 5, 0]", gh.pathTo(4).toString());
+    Assert.assertEquals("[6, 4, 3, 5, 0]", gh.pathTo(6).toString());
   }
 }
