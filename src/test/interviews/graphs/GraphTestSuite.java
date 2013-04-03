9fa9a223557ed4bc4700edd4843a96cb90738932
Francois Rousseau <frncs.rss@gmail.com>
2013-04-03 12:11:47 +0200

    added test suite

---
+++ src/test/interviews/graphs/GraphTestSuite.java
@@ -0,0 +1,20 @@
+package interviews.graphs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test suite.
+ * @author Francois Rousseau
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  ConnectedComponentTest.class,
+  DigraphHandlerTest.class,
+  DigraphTest.class,
+  GraphHandlerTest.class,
+  GraphTest.class,
+  WeightedGraphTest.class
+})
+
+public class GraphTestSuite {}
