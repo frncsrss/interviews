192deccd61d989c62d0f528d682aeda431242577
Francois Rousseau <frncs.rss@gmail.com>
2013-04-03 15:23:49 +0200

    added prim's algorithm (lazy implementation)

--- src/core/interviews/graphs/MinimumSpanningTree.java
+++ src/core/interviews/graphs/MinimumSpanningTree.java
@@ -31,21 +31,56 @@ public class MinimumSpanningTree {
    * Apply Kruskal's algorithm.
    * O(ElogE) to sort the edges by ascending weights.
    * O(ElogE) to find the first V - 1 edges with minimum weights not already connected.
+   * Use constant union-find structure.
    */
   public void kruskal() {
     edges.clear();
 
     PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
-    for(Edge edge: g.edges()) {  // sort them by ascending weight
-      pq.add(edge);
+    for(Edge e: g.edges()) {  // sort them by ascending weight
+      pq.add(e);
     }
 
     UnionFind uf = new QuickUnionPathCompression(g.V);
     while(!pq.isEmpty() && edges.size() < g.V - 1) {  // a mst has V - 1 edges
-      Edge edge = pq.poll();
-      if(!uf.connected(edge.v, edge.w)) {
-        uf.union(edge.v, edge.w);
-        edges.add(edge);
+      Edge e = pq.poll();
+      if(!uf.connected(e.v, e.w)) {
+        uf.union(e.v, e.w);
+        edges.add(e);
+      }
+    }
+  }
+
+  /**
+   * Apply Prim's algorithm.
+   * Maintain a priority queue (min-oriented) of candidate edges.
+   * Lazy implementation (keep in the priority queue edges with both endpoints that are already in
+   * the minimum spanning tree).
+   */
+  public void prim(int v) {
+    edges.clear();
+
+    PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+
+    boolean[] visited = new boolean[g.V];  // is the vertex in the mst
+    visit(0, visited, pq);
+    while(!pq.isEmpty() && edges.size() < g.V - 1) {  // a mst has V - 1 edges
+      Edge e = pq.poll();
+      if(visited[e.v] && visited[e.w]) {  // both endpoints are already in the mst
+        continue;
+      }
+      edges.add(e);
+      if(!visited[e.v]) visit(e.v, visited, pq);
+      if(!visited[e.w]) visit(e.w, visited, pq);
+    }
+  }
+
+
+  private void visit(int v, boolean[] visited, PriorityQueue<Edge> pq) {
+    visited[v] = true;
+    for(Edge e: g.adjE(v)) {
+      if(!visited[e.other(v)]) {
+        pq.add(e);
       }
     }
   }
--- src/test/interviews/graphs/MinimumSpanningTreeTest.java
+++ src/test/interviews/graphs/MinimumSpanningTreeTest.java
@@ -16,4 +16,13 @@ public class MinimumSpanningTreeTest {
         "[0->7(0.16), 2->3(0.17), 1->7(0.19), 0->2(0.26), 5->7(0.28), 4->5(0.35), 6->2(0.4)]",
         mst.edges().toString());
   }
+
+  @Test
+  public void test_prim() {
+    MinimumSpanningTree mst = new MinimumSpanningTree(WeightedGraphTest.setUp());
+    mst.prim(0);
+    Assert.assertEquals(
+        "[0->7(0.16), 1->7(0.19), 0->2(0.26), 2->3(0.17), 5->7(0.28), 4->5(0.35), 6->2(0.4)]",
+        mst.edges().toString());
+  }
 }
