4522575ecba0ea1343ba600f2fd12dca9c0b61f6
Francois Rousseau <frncs.rss@gmail.com>
2013-04-10 15:25:59 +0200

    msd radix sort

--- src/core/interviews/sorts/RadixSort.java
+++ src/core/interviews/sorts/RadixSort.java
@@ -10,7 +10,6 @@ public class RadixSort {
    * Least-significant-digit-first radix sort.
    * Assume an array of fixed-length strings.
    * Run in O(W x n). Stable sort.
-   * @param W length of a string
    * @param R radix (size of the alphabet)
    */
   public static void lsd(String[] arr, int R) {
@@ -25,7 +24,7 @@ public class RadixSort {
         count[arr[i].charAt(d) + 1]++;
       }
       for (int r = 0; r < R; r++) {
-        count[r+1] += count[r];
+        count[r + 1] += count[r];
       }
       for (int i = 0; i < N; i++) {
         aux[count[arr[i].charAt(d)]++] = arr[i];
@@ -34,5 +33,51 @@ public class RadixSort {
         arr[i] = aux[i];
       }
     }
+  }
+
+  /**
+   * Most-significant-digit-first radix sort.
+   * Run in O(W x n). Stable sort.
+   * @param R radix (size of the alphabet)
+   */
+  public static void msd(String[] arr, int R) {
+    String[] aux = new String[arr.length]; 
+    msd(arr, R, aux, 0, arr.length - 1, 0);
+    arr = aux;
+  }
+
+  /**
+   * Internal MSD subroutine that recursively sort a subarray (range lo and hi).
+   */
+  private static void msd(String[] a, int R, String[] aux, int lo, int hi, int d) {
+    if (hi <= lo) return;
+
+    // key-indexed counting for the dth digit
+    int[] count = new int[R + 2];
+    for (int i = lo; i <= hi; i++) {
+      count[charAt(a[i], d) + 2]++;
+    }
+    for (int r = 0; r < R+1; r++) {
+      count[r + 1] += count[r];     
+    }
+    for (int i = lo; i <= hi; i++) {
+      aux[count[charAt(a[i], d) + 1]++] = a[i];
+    }
+    for (int i = lo; i <= hi; i++) {
+      a[i] = aux[i - lo];
+    }
+
+    // sort R subarrays recursively
+    for (int r = 0; r < R; r++) {
+      msd(a, R, aux, lo + count[r], lo + count[r + 1] - 1, d + 1);
+    }
+  }
+
+  /**
+   * Overwrite String.toCharAt so that it returns -1 as last character (like \0 in C).
+   */
+  private static int charAt(String s, int d) {
+   if (d < s.length()) return s.charAt(d);
+   return -1;
   }
 }
--- src/test/interviews/sorts/RadixSortTest.java
+++ src/test/interviews/sorts/RadixSortTest.java
@@ -9,12 +9,24 @@ import org.junit.Test;
  */
 public class RadixSortTest {
   @Test
-  public void test()  {
+  public void test_lsd()  {
     String[] actuals =
         new String[]{"dab", "add", "cab", "fad", "fee", "bad", "dad", "bee", "fed", "bed", "ebb", "ace"};
     RadixSort.lsd(actuals, 256);
     Assert.assertArrayEquals(
         new String[]{"ace", "add", "bad", "bed", "bee", "cab", "dab", "dad", "ebb", "fad", "fed", "fee"},
+        actuals);
+  }
+
+  @Test
+  public void test_msd()  {
+    String[] actuals =
+        new String[]{"she", "sells", "seashells", "by", "the", "sea", "shore", "the", "shells",
+            "she", "sells", "are", "surely", "seashells"};
+    RadixSort.msd(actuals, 256);
+    Assert.assertArrayEquals(
+        new String[]{"are", "by", "sea", "seashells", "seashells", "sells", "sells", "she", "she",
+            "shells", "shore", "surely", "the", "the"},
         actuals);
   }
 }
