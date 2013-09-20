0af9f69a2464f5e6fd59c673f06b16e0a6f568b3
Francois Rousseau <frncs.rss@gmail.com>
2013-09-20 16:54:52 +0200

    [mod] update with O(nk) algorithm

--- src/core/interviews/arrays/ConsecutiveSubsequenceSummingToZero.java
+++ src/core/interviews/arrays/ConsecutiveSubsequenceSummingToZero.java
@@ -1,5 +1,10 @@
 package interviews.arrays;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Print all continuous subsequences of an array that sum to 0.
  * @author Francois Rousseau
@@ -13,24 +18,53 @@ public class ConsecutiveSubsequenceSummingToZero {
     int[] c = new int[n];  // current column of the "matrix"
 
     c[0] = arr[0];
-    checkAndPrint(c[0], arr, 0, 0);
+    if(c[0] == 0) {
+      print(arr, 0, 0);
+    }
     for(int j = 1; j < n; j++) {     // column of the "matrix", end of the interval
       for(int i = 0; i <= j; i++) {  // row of the "matrix", start of the interval
         c[i] = c[i] + arr[j];
-        checkAndPrint(c[i], arr, i, j);
+        if(c[i] == 0) {
+          print(arr, i, j);
+        }
       }
+    }
+  }
+
+  /**
+   * O(n x k) time, O(n) space
+   */
+  public static void f2(int[] arr) {
+    final int n = arr.length;
+    // map of <partial sums, list of indices>
+    Map<Integer, List<Integer>> sums = new HashMap<Integer, List<Integer>>();
+    int sum = 0;
+
+    // insert an artificial -1 for 0 to force the printing the first time we encounter 0
+    List<Integer> indices = new ArrayList<Integer>();
+    indices.add(-1);
+    sums.put(0, indices);
+
+    for(int i = 0; i < n; i++) {
+      sum += arr[i];
+      if(sums.containsKey(sum)) {    // the sum has already been seen
+        for(int j: sums.get(sum)) {  // print all the combinations of indices
+          print(arr, j + 1, i);
+        }
+      } else {
+        sums.put(sum, new ArrayList<Integer>());
+      }
+      sums.get(sum).add(i);
     }
   }
 
   /**
    * O(k) time, O(1) space. Assuming average length of a consecutive subsequence is k.
    */
-  private static void checkAndPrint(int n, int[] arr, int i, int j) {
-    if(n == 0) {
-      for(int k = i; k <= j; k++) {
-        System.out.print(arr[k] + " ");
-      }
-      System.out.println();
+  private static void print(int[] arr, int i, int j) {
+    for(int k = i; k <= j; k++) {
+      System.out.print(arr[k] + " ");
     }
+    System.out.println();
   }
 }
--- src/test/interviews/arrays/ConsecutiveSubsequenceSummingToZeroTest.java
+++ src/test/interviews/arrays/ConsecutiveSubsequenceSummingToZeroTest.java
@@ -1,6 +1,7 @@
 package interviews.arrays;
 
 import static interviews.arrays.ConsecutiveSubsequenceSummingToZero.f;
+import static interviews.arrays.ConsecutiveSubsequenceSummingToZero.f2;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -14,7 +15,7 @@ import org.junit.Test;
  */
 public class ConsecutiveSubsequenceSummingToZeroTest {
   @Test
-  public void test() {
+  public void test_f() {
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(baos);
     System.setOut(ps);
@@ -23,6 +24,79 @@ public class ConsecutiveSubsequenceSummingToZeroTest {
         + "0 -1 -3 4 \n"
         + "-1 -3 4 \n"
         + "0 \n"
+        + "4 0 -4 \n"
+        + "0 -4 1 1 2 \n"
+        + "-4 1 1 2 \n"
+        + "0 -4 1 1 2 0 \n"
+        + "-4 1 1 2 0 \n"
+        + "0 \n"
+        + "1 2 0 -1 -2 \n", baos.toString());
+
+    baos = new ByteArrayOutputStream();
+    ps = new PrintStream(baos);
+    System.setOut(ps);
+    f(new int[] {-1, -3, 4, 5, 4, 0, -4, 1, 1, 2, 0, -1, -2});
+    Assert.assertEquals("-1 -3 4 \n"
+        + "0 \n"
+        + "4 0 -4 \n"
+        + "0 -4 1 1 2 \n"
+        + "-4 1 1 2 \n"
+        + "0 -4 1 1 2 0 \n"
+        + "-4 1 1 2 0 \n"
+        + "0 \n"
+        + "1 2 0 -1 -2 \n", baos.toString());
+
+    baos = new ByteArrayOutputStream();
+    ps = new PrintStream(baos);
+    System.setOut(ps);
+    f(new int[] {4, 0, -4, 1, 1, 2, 0, -1, -2});
+    Assert.assertEquals("0 \n"
+        + "4 0 -4 \n"
+        + "0 -4 1 1 2 \n"
+        + "-4 1 1 2 \n"
+        + "0 -4 1 1 2 0 \n"
+        + "-4 1 1 2 0 \n"
+        + "0 \n"
+        + "1 2 0 -1 -2 \n", baos.toString());
+  }
+
+  @Test
+  public void test_f2() {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    System.setOut(ps);
+    f2(new int[] {0, -1, -3, 4, 5, 4, 0, -4, 1, 1, 2, 0, -1, -2});
+    Assert.assertEquals("0 \n"
+        + "0 -1 -3 4 \n"
+        + "-1 -3 4 \n"
+        + "0 \n"
+        + "4 0 -4 \n"
+        + "0 -4 1 1 2 \n"
+        + "-4 1 1 2 \n"
+        + "0 -4 1 1 2 0 \n"
+        + "-4 1 1 2 0 \n"
+        + "0 \n"
+        + "1 2 0 -1 -2 \n", baos.toString());
+
+    baos = new ByteArrayOutputStream();
+    ps = new PrintStream(baos);
+    System.setOut(ps);
+    f2(new int[] {-1, -3, 4, 5, 4, 0, -4, 1, 1, 2, 0, -1, -2});
+    Assert.assertEquals("-1 -3 4 \n"
+        + "0 \n"
+        + "4 0 -4 \n"
+        + "0 -4 1 1 2 \n"
+        + "-4 1 1 2 \n"
+        + "0 -4 1 1 2 0 \n"
+        + "-4 1 1 2 0 \n"
+        + "0 \n"
+        + "1 2 0 -1 -2 \n", baos.toString());
+
+    baos = new ByteArrayOutputStream();
+    ps = new PrintStream(baos);
+    System.setOut(ps);
+    f2(new int[] {4, 0, -4, 1, 1, 2, 0, -1, -2});
+    Assert.assertEquals("0 \n"
         + "4 0 -4 \n"
         + "0 -4 1 1 2 \n"
         + "-4 1 1 2 \n"
