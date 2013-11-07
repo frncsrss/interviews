d323d96e07f04b23addf061b5ab7851475f4041a
Francois Rousseau <frncs.rss@gmail.com>
2013-11-07 13:46:19 +0100

    [mod] clean up

--- src/core/interviews/arrays/MinInRotatedSortedArray.java
+++ src/core/interviews/arrays/MinInRotatedSortedArray.java
@@ -15,9 +15,6 @@ public class MinInRotatedSortedArray {
     if(arr == null || arr.length == 0) {
       throw new NoSuchElementException();
     }
-    if(arr.length == 1) {
-      return arr[0];
-    }
     for(int i = 0; i < arr.length - 1; i++) {
       if(arr[i + 1] < arr[i]) {
         return arr[i + 1];
@@ -33,9 +30,6 @@ public class MinInRotatedSortedArray {
   public static int f2(int[] arr) {
     if(arr == null || arr.length == 0) {
       throw new NoSuchElementException();
-    }
-    if(arr.length == 1) {
-      return arr[0];
     }
     int lo = 0;
     int hi = arr.length - 1;
