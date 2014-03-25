e108c8eec5844ca5b5d61f5368744c56727b5937
Francois Rousseau <frncs.rss@gmail.com>
2014-03-25 12:20:21 +0100

    [add] Rabin-Karp method for finding anagrams in string

--- src/core/interviews/strings/AnagramsInString.java
+++ src/core/interviews/strings/AnagramsInString.java
@@ -69,4 +69,66 @@ public class AnagramsInString {
     }
     return i;
   }
+
+
+  // prime numbers chosen based on English letter frequency. The more frequent, the lower prime number
+  // http://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+  private static int[] PRIMES = new int[] {
+    2, 41, 37, 47, 3, 67, 71, 23, 5, 101, 61, 17, 19, 13, 31, 43, 97, 29, 11, 7, 73, 83, 79, 89, 59, 53};
+
+  /**
+   * Let n = length(b) and m = length(a).
+   * Time complexity:  O(n + m)
+   * Space complexity: O(n + m)
+   *
+   * Inspired from Rabin-Karp algorithm for substring matching.
+   */
+  public static int f2(String a, String b) {
+    if (a == null || b == null || a.length() > b.length()) {
+      return -1;
+    }
+
+    final int m = a.length();
+    final int n = b.length();
+    final char[] chars = b.toCharArray();
+
+    long golden = hash(a.toCharArray(), m);
+    long h = hash(chars, m);
+
+    if(golden == h) {
+      return 0;
+    }
+
+    for(int i = m; i < n; i++) {
+      h = update(h, chars, m, i);
+      if(golden == h) {
+        return i - m +  1;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Compute the hash of an array s of characters (m first symbols) in linear time.
+   * The hash corresponds to a product of prime numbers.
+   */
+  private static long hash(char[] chars, int m) {
+    long h = 1;
+    for(int i = 0; i < m; i++) {
+      h *= PRIMES[chars[i] - 97];
+    }
+    return h;
+  }
+
+  /**
+   * Update the hash value from s[i - M...i - 1] to s[i - M + 1...i].
+   * @param h the previous hash value
+   * @param chars the array of characters
+   * @param m the length of the hash
+   * @param i the next index to include
+   * @return the new hash value
+   */
+  private static long update(long h, char[] chars, int m, int i) {
+    return h / PRIMES[chars[i - m] - 97] * PRIMES[chars[i] - 97];
+  }
 }
--- src/test/interviews/strings/AnagramsInStringTest.java
+++ src/test/interviews/strings/AnagramsInStringTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
  */
 public class AnagramsInStringTest {
   @Test
-  public void test() {
+  public void test_f() {
     Assert.assertEquals(-1, AnagramsInString.f("xyz",   "afdgksldfm"));
     Assert.assertEquals(0,  AnagramsInString.f("xyz",   "xyzafdgzyxksldfm"));
     Assert.assertEquals(0,  AnagramsInString.f("xyz",   "yxzafdgzyxksldfm"));
@@ -19,5 +19,18 @@ public class AnagramsInStringTest {
     Assert.assertEquals(6,  AnagramsInString.f("abbcd", "xybabdacdbbaskf"));
     Assert.assertEquals(2,  AnagramsInString.f("abbcd", "xyabbcd"));
     Assert.assertEquals(-1, AnagramsInString.f("abbcd", "xyabbc"));
+  }
+
+  @Test
+  public void test_f2() {
+    Assert.assertEquals(-1, AnagramsInString.f2("xyz",   "afdgksldfm"));
+    Assert.assertEquals(0,  AnagramsInString.f2("xyz",   "xyzafdgzyxksldfm"));
+    Assert.assertEquals(0,  AnagramsInString.f2("xyz",   "yxzafdgzyxksldfm"));
+    Assert.assertEquals(4,  AnagramsInString.f2("xyz",   "afdgzyxksldfm"));
+    Assert.assertEquals(8,  AnagramsInString.f2("xyz",   "xyazyfdgzyxksldfm"));
+    Assert.assertEquals(4,  AnagramsInString.f2("abbc",  "xybabacbaskf"));
+    Assert.assertEquals(6,  AnagramsInString.f2("abbcd", "xybabdacdbbaskf"));
+    Assert.assertEquals(2,  AnagramsInString.f2("abbcd", "xyabbcd"));
+    Assert.assertEquals(-1, AnagramsInString.f2("abbcd", "xyabbc"));
   }
 }
