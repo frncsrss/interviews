1ca4e50af17372e401e24555f1ad6798be9e8460
Francois Rousseau <frncs.rss@gmail.com>
2014-03-30 15:44:22 +0200

    [add] general case for an n by m matrix

--- src/core/interviews/arrays/MatrixDiagonals.java
+++ src/core/interviews/arrays/MatrixDiagonals.java
@@ -20,4 +20,20 @@ public class MatrixDiagonals {
       System.out.println();
     }
   }
+
+  /**
+   * Let mat be a n by m matrix.
+   * Time complexity:  O(nm)
+   * Space complexity: O(1)
+   */
+  public static void f2(int[][] mat) {
+    final int n = mat.length;
+    final int m = mat[0].length;
+    for(int k = 0; k < n + m - 1; k++) {
+      for(int j = Math.min(k, m - 1), i = k - j; i <= Math.min(k, n - 1) && j >= 0; i++, j--) {
+        System.out.print(mat[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 }
--- src/test/interviews/arrays/MatrixDiagonalsTest.java
+++ src/test/interviews/arrays/MatrixDiagonalsTest.java
@@ -1,6 +1,7 @@
 package interviews.arrays;
 
 import static interviews.arrays.MatrixDiagonals.f;
+import static interviews.arrays.MatrixDiagonals.f2;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -14,11 +15,25 @@ import org.junit.Test;
  */
 public class MatrixDiagonalsTest {
   @Test
-  public void test() {
+  public void test_f() {
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(baos);
     System.setOut(ps);
     f(new int[][]{new int[]{1, 2, 3}, new int[]{4, 5, 6}, new int[]{7, 8, 9}});
     Assert.assertEquals("1 \n2 4 \n3 5 7 \n6 8 \n9 \n", baos.toString());
+  }
+
+  @Test
+  public void test_f2() {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    System.setOut(ps);
+    f2(new int[][]{new int[]{1, 2, 3, 4}, new int[]{5, 6, 7, 8}});
+    Assert.assertEquals("1 \n2 5 \n3 6 \n4 7 \n8 \n", baos.toString());
+    baos = new ByteArrayOutputStream();
+    ps = new PrintStream(baos);
+    System.setOut(ps);
+    f2(new int[][]{new int[]{1, 2, 3, 4}, new int[]{5, 6, 7, 8}, new int[]{9, 10, 11, 12}});
+    Assert.assertEquals("1 \n2 5 \n3 6 9 \n4 7 10 \n8 11 \n12 \n", baos.toString());
   }
 }
