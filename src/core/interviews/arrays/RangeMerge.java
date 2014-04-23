2277b50b1862e1c5a4a19493869036a427b7a5b4
Francois Rousseau <frncs.rss@gmail.com>
2014-04-23 10:36:10 +0200

    [add] handle the circular case

--- src/core/interviews/arrays/RangeMerge.java
+++ src/core/interviews/arrays/RangeMerge.java
@@ -1,7 +1,7 @@
 package interviews.arrays;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -25,19 +25,42 @@ public class RangeMerge {
     }
   };
 
+  /**
+   * Let n = length(ranges).
+   * Time complexity:  O(nogn)
+   * Space complexity: O(n)
+   */
   public static List<int[]> f(int[][] ranges) {
-    Arrays.sort(ranges, COMPARATOR_START_VALUE);
-    List<int[]> merged = new ArrayList<int[]>();
-    int[] current = ranges[0];
-    for(int i = 1; i < ranges.length; i++) {
-      if(current[1] >= ranges[i][0]) {
-        current[1] = Math.max(current[1], ranges[i][1]);
+
+    // expand any range of the form [a, b] with b < a into [0, b] and [a, Integer.MAX_VALUE]
+    List<int[]> expanded = new ArrayList<int[]>();
+    for(int[] range : ranges) {
+      if(range[0] <= range[1]) {
+        expanded.add(range);
       } else {
-        merged.add(current);
-        current = ranges[i];
+        expanded.add(new int[]{0, range[1]});
+        expanded.add(new int[]{range[0], Integer.MAX_VALUE});
       }
     }
-    merged.add(current);
+    Collections.sort(expanded, COMPARATOR_START_VALUE);
+
+    List<int[]> merged = new ArrayList<int[]>();
+    int[] current = expanded.get(0);
+    for(int i = 1; i < expanded.size(); i++) {
+      if(current[1] >= expanded.get(i)[0] - 1) {  // merge if overlapping or touching
+        current[1] = Math.max(current[1], expanded.get(i)[1]);
+      } else {
+        merged.add(current);
+        current = expanded.get(i);
+      }
+    }
+
+    // we may have to merge the current range with the first if they are touching
+    if(current[1] == Integer.MAX_VALUE && !merged.isEmpty() && merged.get(0)[0] == 0) {
+      merged.get(0)[0] = current[0];
+    } else {
+      merged.add(current);
+    }
     return merged;
   }
 }
--- src/test/interviews/arrays/RangeMergeTest.java
+++ src/test/interviews/arrays/RangeMergeTest.java
@@ -14,7 +14,7 @@ public class RangeMergeTest {
   public void test() {
     Assert.assertArrayEquals(new int[][]{new int[]{0, 100}},
         f(new int[][]{new int[]{0, 50}, new int[]{50, 100}}).toArray());
-    Assert.assertArrayEquals(new int[][]{new int[]{0, 50}, new int[]{51, 100}},
+    Assert.assertArrayEquals(new int[][]{new int[]{0, 100}},
         f(new int[][]{new int[]{0, 50}, new int[]{51, 100}}).toArray());
     Assert.assertArrayEquals(new int[][]{new int[]{0, 100}},
         f(new int[][]{new int[]{0, 75}, new int[]{25, 100}}).toArray());
@@ -22,5 +22,15 @@ public class RangeMergeTest {
         f(new int[][]{new int[]{0, 25}, new int[]{30, 60}, new int[]{50, 100}}).toArray());
     Assert.assertArrayEquals(new int[][]{new int[]{0, 25}, new int[]{30, 100}},
         f(new int[][]{ new int[]{50, 100}, new int[]{30, 60}, new int[]{0, 25}}).toArray());
+    Assert.assertArrayEquals(new int[][]{new int[]{100, 50}},
+        f(new int[][]{new int[]{0, 50}, new int[]{100, 10}}).toArray());
+    Assert.assertArrayEquals(new int[][]{new int[]{0, Integer.MAX_VALUE}},
+        f(new int[][]{new int[]{0, 50}, new int[]{30, 10}}).toArray());
+    Assert.assertArrayEquals(new int[][]{new int[]{100, 30}, new int[]{50, 60}},
+        f(new int[][]{new int[]{0, 30}, new int[]{100, 10}, new int[]{50, 60}}).toArray());
+    Assert.assertArrayEquals(new int[][]{new int[]{Integer.MAX_VALUE - 10, 10}},
+        f(new int[][]{new int[]{Integer.MAX_VALUE - 10, Integer.MAX_VALUE}, new int[]{0, 10}}).toArray());
+    Assert.assertArrayEquals(new int[][]{new int[]{0, Integer.MAX_VALUE}},
+        f(new int[][]{new int[]{0, 50}, new int[]{100, 10}, new int[]{50, 60}, new int[]{60, 0}}).toArray());
   }
 }
