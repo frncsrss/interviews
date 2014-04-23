a5aa827aac156ad6b112c9e9d641258f67792636
Francois Rousseau <frncs.rss@gmail.com>
2014-04-23 18:22:09 +0200

    [doc] dominator problem

--- src/core/interviews/arrays/Dominator.java
+++ src/core/interviews/arrays/Dominator.java
@@ -6,18 +6,31 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Return the most frequent element of an array.
+ * Given an array of elements, it is said to have a dominator (i.e. a majority element) if more
+ * than half of its entries are the same. Given an array, design an efficient algorithm to tell
+ * whether the array has a majority element, and, if so, to find that element. The elements of the
+ * array are not necessarily from some ordered domain like the integers, and so there can be no
+ * comparisons of the form "is A[i] > A[j]?".
+ * However you can answer questions of the form: "is A[i] = A[j]?" in constant time.
+ *
  * @author Francois Rousseau
  */
 public class Dominator {
-  public static int f(int[] A) {
+  /**
+   * Moore's voting algorithm.
+   *
+   * Let n = length(arr).
+   * Time complexity:  O(n)
+   * Space complexity: O(1)
+   */
+  public static int f(int[] arr) {
     int dominator = -1, count = 0;
     // main idea is to cancel values which are different by pairs.
-    for(int i = 0; i < A.length; i++) {
+    for(int i = 0; i < arr.length; i++) {
       if(count == 0) {
         dominator = i;
         count = 1;
-      } else if(A[dominator] == A[i]) {
+      } else if(arr[dominator] == arr[i]) {
         count++;
       } else {
         count--;
@@ -26,8 +39,8 @@ public class Dominator {
     // one more pass to confirm that the value occurs in more than half of the
     // elements of A. [3, 1, 2, 1, 2, 1, 3] would lead to 6 otherwise.
     count = 0;
-    for(int i = 0; i < A.length; i++) {
-      if(A[dominator] == A[i]) {
+    for(int i = 0; i < arr.length; i++) {
+      if(arr[dominator] == arr[i]) {
         count++;
         // force the returned index to be the last one. Easier to test.
         dominator = i;
@@ -38,20 +51,27 @@ public class Dominator {
     // denominator set to 6. If the previous check fails, we should try the next value
     // in the pair (here 3 which is the real denominator).
     // TODO: there may be something I missed in the core algorithm.
-    if(count*2 < A.length && dominator < A.length-1) {
+    if(count*2 < arr.length && dominator < arr.length-1) {
       dominator++;
       count = 0;
-      for(int i = 0; i < A.length; i++) {
-        if(A[dominator] == A[i]) {
+      for(int i = 0; i < arr.length; i++) {
+        if(arr[dominator] == arr[i]) {
           count++;
           // force the returned index to be the last one. Easier to test.
           dominator = i;
         }
       }
     }
-    return (count*2 >= A.length)?dominator:-1;
+    return count * 2 >= arr.length ? dominator : -1;
   }
 
+  /**
+   * Moore's voting algorithm extended to N/k.
+   *
+   * Let n = length(arr).
+   * Time complexity:  O(n)
+   * Space complexity: O(k)
+   */
   public static <T> int f(T[] A, int k) {
     Map<T, Integer> buckets = new HashMap<T, Integer>(k);
     for(int i = 0; i < A.length; i++) {
@@ -59,14 +79,14 @@ public class Dominator {
         buckets.put(A[i], buckets.get(A[i])+1);
       } else {
         if(buckets.size() < k) {
-          buckets.put(A[i], 1);          
+          buckets.put(A[i], 1);
         } else {
           Set<T> keys = new HashSet<T>(buckets.keySet());
           for(T t:keys) {
             if(buckets.get(t) == 1) {
               buckets.remove(t);
             } else {
-              buckets.put(t, buckets.get(t)-1);            
+              buckets.put(t, buckets.get(t)-1);
             }
           }
         }
@@ -75,10 +95,10 @@ public class Dominator {
 
     Map.Entry<T, Integer> dominator = null;
     for (Map.Entry<T, Integer> entry : buckets.entrySet()) {
-        if (dominator == null
-            || entry.getValue().compareTo(dominator.getValue()) >= 0) {
-          dominator = entry;
-        }
+      if (dominator == null
+          || entry.getValue().compareTo(dominator.getValue()) >= 0) {
+        dominator = entry;
+      }
     }
     if (dominator != null) {
       int count = 0;
@@ -90,7 +110,7 @@ public class Dominator {
           dominator.setValue(i);
         }
       }
-      return (count*k >= A.length)?dominator.getValue():-1;
+      return count * k >= A.length ? dominator.getValue() : -1;
     }
     return -1;
   }
--- src/test/interviews/arrays/DominatorTest.java
+++ src/test/interviews/arrays/DominatorTest.java
@@ -1,9 +1,9 @@
 package interviews.arrays;
 
+import static interviews.arrays.Dominator.f;
+
 import org.junit.Assert;
 import org.junit.Test;
-
-import static interviews.arrays.Dominator.f;
 
 /**
  * Test class.
@@ -11,56 +11,14 @@ import static interviews.arrays.Dominator.f;
  */
 public class DominatorTest {
   @Test
-  public void test_good() {
-    int[] arr = {3, 4, 3, 2, 3, -1, 3, 3};
-    Assert.assertEquals(7, f(arr));
-  }
-
-  @Test
-  public void test_good1() {
-    int[] arr = {3, 4, 3, 2, 3, -1, 3, 6};
-    Assert.assertEquals(6, f(arr));
-  }
-
-  @Test
-  public void test_good2() {
-    int[] arr = {2, 4, -1, 6, 3, 3, 3, 3};
-    Assert.assertEquals(7, f(arr));
-  }
-
-  @Test
-  public void test_good3() {
-    int[] arr = {3, 4, 3, 2, 3, -1, 6, 3};
-    Assert.assertEquals(7, f(arr));
-  }
-
-  @Test
-  public void test_good4() {
-    int[] arr = {3, 4, 3, 3, -1, 6, 3, 8};
-    Assert.assertEquals(6, f(arr));
-  }
-
-  @Test
-  public void test_fail() {
-    int[] arr = {3, 1, 2, 1, 2, 1, 3};
-    Assert.assertEquals(-1, f(arr));
-  }
-
-  @Test
-  public void test_empty() {
-    int[] arr = {};
-    Assert.assertEquals(-1, f(arr));
-  }
-
-  @Test
-  public void test_single() {
-    int[] arr = {3};
-    Assert.assertEquals(0, f(arr));
-  }
-
-  @Test
-  public void test_large() {
-    int[] arr = {Integer.MAX_VALUE, 3, Integer.MAX_VALUE};
-    Assert.assertEquals(2, f(arr));
+  public void test() {
+    Assert.assertEquals(7, f(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
+    Assert.assertEquals(6, f(new int[] {3, 4, 3, 2, 3, -1, 3, 6}));
+    Assert.assertEquals(7, f(new int[] {2, 4, -1, 6, 3, 3, 3, 3}));
+    Assert.assertEquals(7, f(new int[] {3, 4, 3, 2, 3, -1, 6, 3}));
+    Assert.assertEquals(6, f(new int[] {3, 4, 3, 3, -1, 6, 3, 8}));
+    Assert.assertEquals(-1,f(new int[] {}));
+    Assert.assertEquals(0, f(new int[] {3}));
+    Assert.assertEquals(2, f(new int[] {Integer.MAX_VALUE, 3, Integer.MAX_VALUE}));
   }
 }
