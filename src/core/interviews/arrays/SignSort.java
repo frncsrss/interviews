d61ecbf7b11bf4389e8f08daaa94e487c645be03
Francois Rousseau <frncs.rss@gmail.com>
2014-07-19 12:52:27 -0700

    [add] blockwise permutation for sign sort

--- src/core/interviews/arrays/SignSort.java
+++ src/core/interviews/arrays/SignSort.java
@@ -34,6 +34,67 @@ public class SignSort {
     return arr;
   }
 
+  /**
+   * In-place stable sign sort. Return the arr for ease of testing.
+   *
+   * The key idea is given an array of the form N1P1N2P2 with Ni being a block of negative integers
+   * and Pi a block of positive integers and the reverse block-operation B --> B' then:
+   * N1P1N2P2 --> N1P1'N2'P2 --> N1(P1'N2')'P2 == N1N2P1P2, which is stable
+   * 
+   * N1 and P2 can be of size 0. If P1 is empty then there are no positive elements. If N2 is empty
+   * then we are done. Otherwise, we continue with N1 = N1N2 and P1 = P1P2.
+   *
+   * Let n = length(arr).
+   * Time complexity:  O(n^2)
+   * Space complexity: O(1)
+   */
+  public static int[] f2(int [] arr) {
+    if(arr == null || arr.length < 2) {
+      return arr;
+    }
+
+    final int n = arr.length;
+
+    int p1 = 0;
+    while(true) {
+      while(p1 < n && arr[p1] < 0) {
+        p1++;
+      }  // p1, index of first positive
+
+      if(p1 == n) {  // only negative elements
+        return arr;
+      }
+
+      int n2 = p1 + 1;
+      while(n2 < n && arr[n2] > 0) {
+        n2++;
+      }  // n2, index of second negative
+
+      if(n2 == n) {  // no second negative block remaining
+        return arr;
+      }
+
+      int p2 = n2 + 1;
+      while(p2 < n && arr[p2] < 0) {
+        p2++;
+      }  // n2, index of second positive
+
+      reverse(arr, p1, n2 - 1);
+      reverse(arr, n2, p2 - 1);
+      reverse(arr, p1, p2 - 1);
+    }
+  }
+
+  /**
+   * Reverse the array between the inclusive indices lo and hi.
+   */
+  private static void reverse(int[] arr, int lo, int hi) {
+    int mid = lo + hi >>> 1;
+      for(int i = lo; i <= mid; i++) {
+        swap(arr, i, hi + lo - i);
+      }
+  }
+
   private static void swap(int[] arr, int i, int j) {
     if(i == j) {
       return;
--- src/test/interviews/arrays/SignSortTest.java
+++ src/test/interviews/arrays/SignSortTest.java
@@ -1,6 +1,7 @@
 package interviews.arrays;
 
 import static interviews.arrays.SignSort.f;
+import static interviews.arrays.SignSort.f2;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,7 +12,7 @@ import org.junit.Test;
  */
 public class SignSortTest {
   @Test
-  public void test() {
+  public void test_f() {
     Assert.assertArrayEquals(new int[]{-2, -3, -2, 1, 4, 5}, f(new int[]{1, -2, 4, -3, -2, 5}));
     Assert.assertArrayEquals(new int[]{1, 2, 4, 3, 2, 5}, f(new int[]{1, 2, 4, 3, 2, 5}));
     Assert.assertArrayEquals(new int[]{-1, -2, -4, -3, -2, -5}, f(new int[]{-1, -2, -4, -3, -2, -5}));
@@ -19,5 +20,16 @@ public class SignSortTest {
     Assert.assertArrayEquals(new int[]{-1, -2, -4, -1, 3, 2, 5}, f(new int[]{-1, -2, -4, 3, 2, 5, -1}));
     Assert.assertArrayEquals(new int[]{-1, -2, -3, -4, 1, 2, 3, 4, 5, 6, 7},
         f(new int[]{1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4}));
+  }
+
+  @Test
+  public void test_f2() {
+    Assert.assertArrayEquals(new int[]{-1, -2, 1, 2}, f2(new int[]{-1, 1, -2, 2}));
+    Assert.assertArrayEquals(new int[]{-2, 1, 2}, f2(new int[]{1, -2, 2}));
+    Assert.assertArrayEquals(new int[]{-2, 1}, f2(new int[]{1, -2}));
+    Assert.assertArrayEquals(new int[]{-1, 1}, f2(new int[]{-1, 1}));
+    Assert.assertArrayEquals(new int[]{-1, -2, -3, 1, 2, 3}, f2(new int[]{-1, 1, -2, 2, -3, 3}));
+    Assert.assertArrayEquals(new int[]{-1, -2, -3, -4, 1, 2, 3, 4, 5, 6, 7},
+        f2(new int[]{1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4}));
   }
 }
