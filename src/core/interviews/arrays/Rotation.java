907fd950229d159a44c34e89e03170e172a1b59e
Francois Rousseau <frncs.rss@gmail.com>
2014-07-19 12:52:09 -0700

    [mod] clean up

--- src/core/interviews/arrays/Rotation.java
+++ src/core/interviews/arrays/Rotation.java
@@ -1,66 +1,44 @@
 package interviews.arrays;
 
-import java.util.List;
-
 /**
- * Circular permutation of a list/array.
+ * In-place rotation (shift) of a circular array by an offset of d.
+ *
+ * Example: f([7, 1, 5, 2, 4, 3, 0], 3) = [4, 3, 0, 7, 1, 5, 2]
+ * 
+ * The array is returned for testing purposes.
  *
  * @author Francois Rousseau
  */
 public class Rotation {
-  public static <E> List<E> reversalAlgorithm1(List<E> list, int d) {
-    final int n = list.size();
-    d = d % n;  // permutation is done modulo n
-    if(n == 0 || d == 0) {
-      return list;  // for testing purposes
-    }
-    reverse(list, 0, n - 1);
-    reverse(list, 0, d - 1);
-    reverse(list, d, n - 1);
-    return list;  // for testing purposes
-  }
-
-  public static <E> E[] reversalAlgorithm1(E[] arr, int d) {
+  public static int[] reversalAlgorithm1(int[] arr, int d) {
     final int n = arr.length;
     d = d % n;  // permutation is done modulo n
     if(n == 0 || d == 0) {
-      return arr;  // for testing purposes
+      return arr;
     }
     reverse(arr, 0, n - 1);
     reverse(arr, 0, d - 1);
     reverse(arr, d, n - 1);
-    return arr;  // for testing purposes
+    return arr;
   }
 
-  public static <E> List<E> reversalAlgorithm2(List<E> list, int d) {
-    final int n = list.size();
-    d = d % n;  // permutation is done modulo n
-    if(n == 0 || d == 0) {
-      return list;  // for testing purposes
-    }
-    reverse(list, n - d, n - 1);
-    reverse(list, 0, n - d - 1);
-    reverse(list, 0, n - 1);
-    return list;  // for testing purposes
-  }
-
-  public static <E> E[] reversalAlgorithm2(E[] arr, int d) {
+  public static int[] reversalAlgorithm2(int[] arr, int d) {
     final int n = arr.length;
     d = d % n;  // permutation is done modulo n
     if(n == 0 || d == 0) {
-      return arr;  // for testing purposes
+      return arr;
     }
     reverse(arr, n - d, n - 1);
     reverse(arr, 0, n - d - 1);
     reverse(arr, 0, n - 1);
-    return arr;  // for testing purposes
+    return arr;
   }
 
   public static int[] blockSwap(int[] arr, int d) {
     final int n = arr.length;
     d = d % n;  // permutation is done modulo n
     if(n == 0 || d == 0) {
-      return arr;  // for testing purposes
+      return arr;
     }
     int i = d;
     int j = n-d;
@@ -75,18 +53,10 @@ public class Rotation {
     }
     // Finally, block swap A and B
     Swap.f(arr, d - i, d, i);
-    return arr;  // for testing purposes
+    return arr;
   }
 
-
-  private static <E> void reverse(List<E> list, int start, int end) {
-    final int middle = start + end >>> 1;
-    for(int i = start; i < middle + 1; i++) {
-      Swap.f(list, i, start + end - i);
-    }
-  }
-
-  private static <E> void reverse(E[] arr, int start, int end) {
+  private static void reverse(int[] arr, int start, int end) {
     while(start < end) {
       Swap.f(arr, start++, end--);
     }
--- src/test/interviews/arrays/RotationTest.java
+++ src/test/interviews/arrays/RotationTest.java
@@ -4,10 +4,6 @@ import static interviews.arrays.Rotation.blockSwap;
 import static interviews.arrays.Rotation.reversalAlgorithm1;
 import static interviews.arrays.Rotation.reversalAlgorithm2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,98 +14,98 @@ import org.junit.Test;
 public class RotationTest {
   @Test
   public void test_reversalAlgorithm1() {
-    Assert.assertEquals(
-        Arrays.asList(7, 1, 5, 2, 4, 3, 0),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 0));
-    Assert.assertEquals(
-        Arrays.asList(0, 7, 1, 5, 2, 4, 3),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 1));
-    Assert.assertEquals(
-        Arrays.asList(3, 0, 7, 1, 5, 2, 4),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 2));
-    Assert.assertEquals(
-        Arrays.asList(4, 3, 0, 7, 1, 5, 2),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 3));
-    Assert.assertEquals(
-        Arrays.asList(2, 4, 3, 0, 7, 1, 5),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 4));
-    Assert.assertEquals(
-        Arrays.asList(5, 2, 4, 3, 0, 7, 1),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 5));
-    Assert.assertEquals(
-        Arrays.asList(1, 5, 2, 4, 3, 0, 7),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 6));
-    Assert.assertEquals(
-        Arrays.asList(7, 1, 5, 2, 4, 3, 0),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 7));
-    Assert.assertEquals(
-        Arrays.asList(0, 7, 1, 5, 2, 4, 3),
-        reversalAlgorithm1(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 8));
+    Assert.assertArrayEquals(
+        new int[]{7, 1, 5, 2, 4, 3, 0},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 0));
+    Assert.assertArrayEquals(
+        new int[]{0, 7, 1, 5, 2, 4, 3},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 1));
+    Assert.assertArrayEquals(
+        new int[]{3, 0, 7, 1, 5, 2, 4},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 2));
+    Assert.assertArrayEquals(
+        new int[]{4, 3, 0, 7, 1, 5, 2},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 3));
+    Assert.assertArrayEquals(
+        new int[]{2, 4, 3, 0, 7, 1, 5},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 4));
+    Assert.assertArrayEquals(
+        new int[]{5, 2, 4, 3, 0, 7, 1},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 5));
+    Assert.assertArrayEquals(
+        new int[]{1, 5, 2, 4, 3, 0, 7},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 6));
+    Assert.assertArrayEquals(
+        new int[]{7, 1, 5, 2, 4, 3, 0},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 7));
+    Assert.assertArrayEquals(
+        new int[]{0, 7, 1, 5, 2, 4, 3},
+        reversalAlgorithm1(new int[]{7, 1, 5, 2, 4, 3, 0}, 8));
   }
 
   @Test
   public void test_reversalAlgorithm1_big() {
     final int N = 1000000;
-    List<Integer> input = new ArrayList<Integer>(N);
+    int[] input = new int[N];
     for(int i = 0; i < N; i++) {
-      input.add(i);
+      input[i] = i;
     }
-    List<Integer> expected = new ArrayList<Integer>(N);
+    int[] expected = new int[N];
     for(int i = N/2; i < N; i++) {
-      expected.add(i);
+      expected[i - N/2] = i;
     }
     for(int i = 0; i < N/2; i++) {
-      expected.add(i);
+      expected[N/2 + i] = i;
     }
-    Assert.assertEquals(expected, reversalAlgorithm1(input, N/2));
+    Assert.assertArrayEquals(expected, reversalAlgorithm1(input, N/2));
   }
 
   @Test
   public void test_reversalAlgorithm2() {
-    Assert.assertEquals(
-        Arrays.asList(7, 1, 5, 2, 4, 3, 0),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 0));
-    Assert.assertEquals(
-        Arrays.asList(0, 7, 1, 5, 2, 4, 3),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 1));
-    Assert.assertEquals(
-        Arrays.asList(3, 0, 7, 1, 5, 2, 4),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 2));
-    Assert.assertEquals(
-        Arrays.asList(4, 3, 0, 7, 1, 5, 2),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 3));
-    Assert.assertEquals(
-        Arrays.asList(2, 4, 3, 0, 7, 1, 5),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 4));
-    Assert.assertEquals(
-        Arrays.asList(5, 2, 4, 3, 0, 7, 1),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 5));
-    Assert.assertEquals(
-        Arrays.asList(1, 5, 2, 4, 3, 0, 7),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 6));
-    Assert.assertEquals(
-        Arrays.asList(7, 1, 5, 2, 4, 3, 0),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 7));
-    Assert.assertEquals(
-        Arrays.asList(0, 7, 1, 5, 2, 4, 3),
-        reversalAlgorithm2(Arrays.asList(7, 1, 5, 2, 4, 3, 0), 8));
+    Assert.assertArrayEquals(
+        new int[]{7, 1, 5, 2, 4, 3, 0},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 0));
+    Assert.assertArrayEquals(
+        new int[]{0, 7, 1, 5, 2, 4, 3},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 1));
+    Assert.assertArrayEquals(
+        new int[]{3, 0, 7, 1, 5, 2, 4},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 2));
+    Assert.assertArrayEquals(
+        new int[]{4, 3, 0, 7, 1, 5, 2},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 3));
+    Assert.assertArrayEquals(
+        new int[]{2, 4, 3, 0, 7, 1, 5},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 4));
+    Assert.assertArrayEquals(
+        new int[]{5, 2, 4, 3, 0, 7, 1},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 5));
+    Assert.assertArrayEquals(
+        new int[]{1, 5, 2, 4, 3, 0, 7},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 6));
+    Assert.assertArrayEquals(
+        new int[]{7, 1, 5, 2, 4, 3, 0},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 7));
+    Assert.assertArrayEquals(
+        new int[]{0, 7, 1, 5, 2, 4, 3},
+        reversalAlgorithm2(new int[]{7, 1, 5, 2, 4, 3, 0}, 8));
   }
 
   @Test
   public void test_reversalAlgorithm2_big() {
     final int N = 1000000;
-    List<Integer> input = new ArrayList<Integer>(N);
+    int[] input = new int[N];
     for(int i = 0; i < N; i++) {
-      input.add(i);
+      input[i] = i;
     }
-    List<Integer> expected = new ArrayList<Integer>(N);
+    int[] expected = new int[N];
     for(int i = N/2; i < N; i++) {
-      expected.add(i);
+      expected[i - N/2] = i;
     }
     for(int i = 0; i < N/2; i++) {
-      expected.add(i);
+      expected[N/2 + i] = i;
     }
-    Assert.assertEquals(expected, reversalAlgorithm2(input, N/2));
+    Assert.assertArrayEquals(expected, reversalAlgorithm2(input, N/2));
   }
 
   @Test
