d29bfd797c775b4fa7418b7f54a261bb54a3e1d1
Francois Rousseau <frncs.rss@gmail.com>
2014-07-19 19:15:05 -0700

    [mod] linearithmic sign sort (mergesort)

--- src/core/interviews/arrays/SignSort.java
+++ src/core/interviews/arrays/SignSort.java
@@ -35,17 +35,17 @@ public class SignSort {
   }
 
   /**
-   * In-place stable sign sort. Return the arr for ease of testing.
+   * In-place stable sign sort (mergesort). Return the arr for ease of testing.
    *
    * The key idea is given an array of the form N1P1N2P2 with Ni being a block of negative integers
    * and Pi a block of positive integers and the reverse block-operation B --> B' then:
-   * N1P1N2P2 --> N1P1'N2'P2 --> N1(P1'N2')'P2 == N1N2P1P2, which is stable
+   * N1P1N2P2 --> N1P1'N2'P2 --> N1(P1'N2')'P2 == N1N2P1P2, which is stable (merge operation).
    *
    * N1 and P2 can be of size 0. If P1 is empty then there are no positive elements. If N2 is empty
    * then we are done. Otherwise, we continue with N1 = N1N2 and P1 = P1P2.
    *
    * Let n = length(arr).
-   * Time complexity:  O(n^2)
+   * Time complexity:  O(nlogn) (T(n) = 2T(n/2) + O(n))
    * Space complexity: O(1)
    */
   public static int[] f2(int [] arr) {
@@ -53,36 +53,55 @@ public class SignSort {
       return arr;
     }
 
-    final int n = arr.length;
+    f2(arr, 0, arr.length - 1);
 
-    int p1 = 0;
-    while(true) {
-      while(p1 < n && arr[p1] < 0) {
-        p1++;
-      }  // p1, index of first positive
+    return arr;
+  }
 
-      if(p1 == n) {  // only negative elements
-        return arr;
-      }
-
-      int n2 = p1 + 1;
-      while(n2 < n && arr[n2] > 0) {
-        n2++;
-      }  // n2, index of second negative
-
-      if(n2 == n) {  // no second negative block remaining
-        return arr;
-      }
-
-      int p2 = n2 + 1;
-      while(p2 < n && arr[p2] < 0) {
-        p2++;
-      }  // n2, index of second positive
-
-      reverse(arr, p1, n2 - 1);
-      reverse(arr, n2, p2 - 1);
-      reverse(arr, p1, p2 - 1);
+  private static void f2(int[] arr, int lo, int hi) {
+    if(lo == hi) {
+      return;
     }
+    int mid = lo + hi >>> 1;
+    f2(arr, lo, mid);
+    f2(arr, mid + 1, hi);
+    merge(arr, lo, hi);
+  }
+
+  /**
+   * In-place merge of the array between the inclusive indices lo and hi. From N1P1N2P2 to N1N2P1P2.
+   *
+   * Let n = hi - lo + 1.
+   * Time complexity:  O(n)
+   * Space complexity: O(1)
+   */
+  private static void merge(int[] arr, int lo, int hi) {
+    int p1 = lo;
+    while(p1 <= hi && arr[p1] < 0) {
+      p1++;
+    }  // p1, index of first positive
+
+    if(p1 > hi) {  // only negative elements
+      return;
+    }
+
+    int n2 = p1 + 1;
+    while(n2 <= hi && arr[n2] > 0) {
+      n2++;
+    }  // n2, index of second negative
+
+    if(n2 > hi) {  // no second negative block remaining
+      return;
+    }
+
+    int p2 = n2 + 1;
+    while(p2 <= hi && arr[p2] < 0) {
+      p2++;
+    }  // n2, index of second positive
+
+    reverse(arr, p1, n2 - 1);
+    reverse(arr, n2, p2 - 1);
+    reverse(arr, p1, p2 - 1);
   }
 
   /**
@@ -90,9 +109,9 @@ public class SignSort {
    */
   private static void reverse(int[] arr, int lo, int hi) {
     int mid = lo + hi >>> 1;
-      for(int i = lo; i <= mid; i++) {
-        swap(arr, i, hi + lo - i);
-      }
+    for(int i = lo; i <= mid; i++) {
+      swap(arr, i, hi + lo - i);
+    }
   }
 
   private static void swap(int[] arr, int i, int j) {
