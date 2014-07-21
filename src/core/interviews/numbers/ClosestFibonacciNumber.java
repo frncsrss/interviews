af3e43f6ba2f59bb3bb0a653366fb19550afc8ce
Francois Rousseau <frncs.rss@gmail.com>
2014-07-20 17:05:56 -0700

    [add] binary search in pre-filled array

--- src/core/interviews/numbers/ClosestFibonacciNumber.java
+++ src/core/interviews/numbers/ClosestFibonacciNumber.java
@@ -1,11 +1,18 @@
 package interviews.numbers;
 
+import java.util.Arrays;
+
 /**
  * Find the smallest number of steps needed to change a given integer into a Fibonacci number.
+ *
+ * 1 <= n <= 1,000,000
  *
  * @author Francois Rousseau
  */
 public class ClosestFibonacciNumber {
+  /**
+   * At most 32 steps.
+   */
   public static int f(int n) {
     if(n == 0 || n == 1 || n == 2) {
       return 0;
@@ -21,4 +28,23 @@ public class ClosestFibonacciNumber {
       f_1 = f;
     }
   }
+
+  private static final int[] NUMBERS = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+    987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229,
+    832040, 1346269};
+
+  /**
+   * At most 5 steps.
+   */
+  public static int f2(int n) {
+    if(n == 0 || n == 1 || n == 2) {
+      return 0;
+    }
+    int index = Arrays.binarySearch(NUMBERS, n);
+    if(index < 0) {
+      return Math.min(Math.abs(n - NUMBERS[-index - 1]), Math.abs(n - NUMBERS[-index - 2]));
+    }
+    return 0;
+  }
+
 }
--- src/test/interviews/numbers/ClosestFibonacciNumberTest.java
+++ src/test/interviews/numbers/ClosestFibonacciNumberTest.java
@@ -1,6 +1,7 @@
 package interviews.numbers;
 
 import static interviews.numbers.ClosestFibonacciNumber.f;
+import static interviews.numbers.ClosestFibonacciNumber.f2;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,11 +12,20 @@ import org.junit.Test;
  */
 public class ClosestFibonacciNumberTest {
   @Test
-  public void test() {
+  public void test_f() {
     Assert.assertEquals(0, f(1));
     Assert.assertEquals(0, f(13));
     Assert.assertEquals(2, f(15));
     Assert.assertEquals(2, f(19));
     Assert.assertEquals(167960, f(1000000));
+  }
+
+  @Test
+  public void test_f2() {
+    Assert.assertEquals(0, f2(1));
+    Assert.assertEquals(0, f2(13));
+    Assert.assertEquals(2, f2(15));
+    Assert.assertEquals(2, f2(19));
+    Assert.assertEquals(167960, f2(1000000));
   }
 }
