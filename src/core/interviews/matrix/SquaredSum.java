d5aeec82b7cc6d8fab40383f8ac3d61c5e02aec1
Francois Rousseau <frncs.rss@gmail.com>
2014-08-09 14:35:39 -0700

    [new] matrix with sum of square

---
+++ src/core/interviews/matrix/SquaredSum.java
@@ -0,0 +1,35 @@
+package interviews.matrix;
+
+/**
+ * Given a matrix A, return a matrix S such that S[i][j] = sum_{0 <= k,l <= i,j}{A[k][l]}.
+ *
+ * @author Francois Rousseau
+ */
+public class SquaredSum {
+  /**
+   * Let A be a n by m matrix.
+   * Time complexity:  O(nm)
+   * Space complexity: O(1)  // omitting the returned matrix
+   */
+  public static int[][] f(int[][] A) {
+    final int n = A.length;
+    final int m = A[0].length;
+    int[][] S = new int[n][m];
+
+    int sum = 0;
+    for(int j = 0; j < m; j++) {
+      sum += A[0][j];
+      S[0][j] = sum;
+    }
+
+    for(int i = 1; i < n; i++) {
+      sum = 0;
+      for(int j = 0; j < m; j++) {
+        sum += A[i][j];
+        S[i][j] = sum + S[i - 1][j];
+      }
+    }
+
+    return S;
+  }
+}
