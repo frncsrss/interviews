83e2fde5d9873afefc5a0537bb274e396c887c42
Francois Rousseau <frncs.rss@gmail.com>
2014-08-10 10:54:16 -0700

    [new] set to 0 matrix rows and columns

---
+++ src/core/interviews/matrix/ZeroRowsAndColumns.java
@@ -0,0 +1,128 @@
+package interviews.matrix;
+
+
+/**
+ * Given a matrix A, set to 0 any cell for which there is a 0 in its row or column. The operation
+ * has to be in-place.
+ *
+ * @author Francois Rousseau
+ */
+public class ZeroRowsAndColumns {
+  private final static int RESERVED = 1;
+
+  /**
+   * Let A be a n by m matrix.
+   * Time complexity:  O(nm)
+   * Space complexity: O(1)
+   */
+  public static int[][] f(int[][] A) {
+    final int n = A.length;
+    final int m = A[0].length;
+
+    boolean flipBack = false;
+
+    if(A[0][0] == 0) {
+      A[0][0] = RESERVED;
+    } else if(A[0][0] == RESERVED) {
+      A[0][0] = RESERVED + 1;
+      flipBack = true;
+      for(int i = 1; i < n; i++) {
+        if(A[i][0] == 0) {
+          flipBack = false;
+        }
+      }
+      for(int j = 1; j < m; j++) {
+        if(A[0][j] == 0) {
+          flipBack = false;
+        }
+      }
+    }
+
+    for(int i = 0; i < n; i++) {
+      int last = -1;
+      for(int j = 0; j < m; j++) {
+        if(A[i][j] != 0) {
+          continue;
+        }
+        setLeftAndUp(A, i, j, last);
+        A[i][j] = RESERVED;
+        last = j;
+      }
+    }
+
+    for(int i = n - 1; i >= 0; i--) {
+      int last = m;
+      for(int j = m - 1; j >= 0; j--) {
+        if(!isReservedLeftAndUp(A, i, j)) {
+          continue;
+        }
+        setRightAndDown(A, n, m, i, j, last);
+        last = j;
+      }
+    }
+
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        if(isReservedLeftAndUp(A, i, j)) {
+          A[i][j] = 0;
+        }
+      }
+    }
+
+    if(flipBack) {
+      A[0][0] = RESERVED;
+    }
+
+    return A;  // for ease of testing
+  }
+
+  private static void setLeftAndUp(int[][] A, int i, int j, int last) {
+    for(int k = i - 1; k >= 0; k--) {
+      if(isReservedLeftAndUp(A, k, j)) {
+        break;
+      }
+      A[k][j] = 0;
+    }
+    for(int l = j - 1; l > last; l--) {
+      A[i][l] = 0;
+    }
+  }
+
+  private static void setRightAndDown(int[][] A, int n, int m, int i, int j, int last) {
+    for(int k = i + 1; k < n; k++) {
+      if(isReservedRightAndDown(A, n, m, k, j)) {
+        break;
+      }
+      A[k][j] = 0;
+    }
+    for(int l = j + 1; l < last; l++) {
+      A[i][l] = 0;
+    }
+  }
+
+  private static boolean isReservedLeftAndUp(int[][] A, int i, int j) {
+    if(i == 0 && j == 0) {
+      return A[i][j] == RESERVED;
+    }
+    if(i > 0 && j == 0) {
+      return A[i][j] == RESERVED && A[i - 1][j] == 0;
+    }
+    if(i == 0 && j > 0) {
+      return A[i][j] == RESERVED && A[i][j - 1] == 0;
+    }
+    return A[i][j] == RESERVED && A[i - 1][j] == 0 && A[i][j - 1] == 0;
+  }
+
+  private static boolean isReservedRightAndDown(int[][] A, int n, int m, int i, int j) {
+    if(i == n - 1 && j == m - 1) {
+      return A[i][j] == RESERVED;
+    }
+    if(i < n - 1 && j == m - 1) {
+      return A[i][j] == RESERVED && A[i + 1][j] == 0;
+    }
+    if(i == n - 1 && j < m - 1) {
+      return A[i][j] == RESERVED && A[i][j + 1] == 0;
+    }
+    return A[i][j] == RESERVED && A[i + 1][j] == 0 && A[i][j + 1] == 0;
+  }
+}
---
+++ src/test/interviews/matrix/ZeroRowsAndColumnsTest.java
@@ -0,0 +1,79 @@
+package interviews.matrix;
+
+import static interviews.matrix.ZeroRowsAndColumns.f;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test class.
+ * @author Francois Rousseau
+ */
+public class ZeroRowsAndColumnsTest {
+  @Test
+  public void test_f() {
+    Assert.assertArrayEquals(new int[][]{new int[]{0}}, f(new int[][]{new int[]{0}}));
+    Assert.assertArrayEquals(new int[][]{new int[]{1}}, f(new int[][]{new int[]{1}}));
+    Assert.assertArrayEquals(new int[][]{new int[]{2}}, f(new int[][]{new int[]{2}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{1, 2}, new int[]{3, 4}},
+        f(new int[][]{new int[]{1, 2}, new int[]{3, 4}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{3, 0}},
+        f(new int[][]{new int[]{1, 0}, new int[]{3, 4}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{0, 4}},
+        f(new int[][]{new int[]{0, 1}, new int[]{3, 4}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{0, 0}},
+        f(new int[][]{new int[]{0, 1}, new int[]{3, 0}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{0, 0}},
+        f(new int[][]{new int[]{1, 2}, new int[]{0, 0}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 0, 0, 0, 0, 0},
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 0, 0, 0, 0, 0}
+        },
+        f(new int[][]{
+            new int[]{1, 2, 0, 3, 4, 0}, new int[]{5, 6, 7, 0, 8, 9},
+            new int[]{1, 2, 0, 3, 4, 5}, new int[]{6, 0, 7, 8, 9, 1}
+        }));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{5, 0, 0, 3, 8, 9},
+            new int[]{1, 0, 0, 3, 4, 5}, new int[]{0, 0, 0, 0, 0, 0}
+        },
+        f(new int[][]{
+            new int[]{1, 2, 0, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
+            new int[]{1, 2, 7, 3, 4, 5}, new int[]{6, 0, 7, 8, 9, 1}
+        }));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{1, 2, 0, 3, 4, 2}, new int[]{5, 6, 0, 3, 8, 9},
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{6, 2, 0, 8, 9, 1}
+        },
+        f(new int[][]{
+            new int[]{1, 2, 1, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
+            new int[]{1, 2, 0, 3, 4, 5}, new int[]{6, 2, 7, 8, 9, 1}
+        }));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 6, 7, 3, 8, 9},
+            new int[]{0, 2, 7, 3, 4, 5}, new int[]{0, 2, 7, 8, 9, 1}
+        },
+        f(new int[][]{
+            new int[]{0, 2, 1, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
+            new int[]{1, 2, 7, 3, 4, 5}, new int[]{6, 2, 7, 8, 9, 1}
+        }));
+  }
+}
