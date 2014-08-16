78022d65d9126e0bd80099d57acc4eef97658420
Francois Rousseau <frncs.rss@gmail.com>
2014-08-16 14:15:37 -0700

    [add] square matrix rotation (in-place)

--- src/core/interviews/matrices/Rotation.java
+++ src/core/interviews/matrices/Rotation.java
@@ -1,5 +1,6 @@
 package interviews.matrices;
 
+
 /**
  * Given a matrix A, rotate it by 90û left/right. In-place if square matrix.
  *
@@ -42,5 +43,54 @@ public class Rotation {
     }
 
     return B;
+  }
+
+  /**
+   * Let A be a n by n matrix.
+   * Time complexity:  O(n^2)
+   * Space complexity: O(1)  // in-place
+   */
+  public static int[][] leftSquare(int[][] A) {
+    final int n = A.length;
+    assert n == A[0].length;
+
+    for(int i = 0; i < n / 2; i++) {
+      for(int j = 0; j < Math.ceil(n / 2.0); j++) {
+        swap(A, i, j, n - 1 - j, i);
+        swap(A, i, j, n - 1 - i, n - 1 - j);
+        swap(A, i, j, j, n - 1 - i);
+      }
+    }
+
+    return A;  // for ease of testing
+  }
+
+  /**
+   * Let A be a n by n matrix.
+   * Time complexity:  O(n^2)
+   * Space complexity: O(1)  // in-place
+   */
+  public static int[][] rightSquare(int[][] A) {
+    final int n = A.length;
+    assert n == A[0].length;
+
+    for(int i = 0; i < n / 2; i++) {
+      for(int j = 0; j < Math.ceil(n / 2.0); j++) {
+        swap(A, i, j, j, n - 1 - i);
+        swap(A, i, j, n - 1 - i, n - 1 - j);
+        swap(A, i, j, n - 1 - j, i);
+      }
+    }
+
+    return A;  // for ease of testing
+  }
+
+  private static void swap(int[][] A, int i1, int j1, int i2, int j2) {
+    if(i1 == i2 && j1 == j2) {
+      return;
+    }
+    A[i1][j1] ^= A[i2][j2];
+    A[i2][j2] ^= A[i1][j1];
+    A[i1][j1] ^= A[i2][j2];
   }
 }
--- src/test/interviews/matrices/RotationTest.java
+++ src/test/interviews/matrices/RotationTest.java
@@ -1,7 +1,9 @@
 package interviews.matrices;
 
 import static interviews.matrices.Rotation.left;
+import static interviews.matrices.Rotation.leftSquare;
 import static interviews.matrices.Rotation.right;
+import static interviews.matrices.Rotation.rightSquare;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -50,6 +52,76 @@ public class RotationTest {
             new int[]{1, 2, 3, 4, 5, 6},
             new int[]{7, 8, 9, 10, 11, 12},
             new int[]{13, 14, 15, 16, 17, 18},
+        }));
+  }
+
+  @Test
+  public void test_leftSquare() {
+    Assert.assertArrayEquals(new int[][]{new int[]{0}}, leftSquare(new int[][]{new int[]{0}}));
+    Assert.assertArrayEquals(new int[][]{new int[]{1, 1}, new int[]{0, 0}},
+        leftSquare(new int[][]{new int[]{0, 1}, new int[]{0, 1}}));
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{4, 8, 12, 16},
+            new int[]{3, 7, 11, 15},
+            new int[]{2, 6, 10, 14},
+            new int[]{1, 5, 9, 13},
+        },
+        leftSquare(new int[][]{
+            new int[]{1, 2, 3, 4},
+            new int[]{5, 6, 7, 8},
+            new int[]{9, 10, 11, 12},
+            new int[]{13, 14, 15, 16},
+        }));
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{5, 10, 15, 20, 25},
+            new int[]{4, 9, 14, 19, 24},
+            new int[]{3, 8, 13, 18, 23},
+            new int[]{2, 7, 12, 17, 22},
+            new int[]{1, 6, 11, 16, 21},
+        },
+        leftSquare(new int[][]{
+            new int[]{1, 2, 3, 4, 5},
+            new int[]{6, 7, 8, 9, 10},
+            new int[]{11, 12, 13, 14, 15},
+            new int[]{16, 17, 18, 19, 20},
+            new int[]{21, 22, 23, 24, 25},
+        }));
+  }
+
+  @Test
+  public void test_rightSquare() {
+    Assert.assertArrayEquals(new int[][]{new int[]{0}}, rightSquare(new int[][]{new int[]{0}}));
+    Assert.assertArrayEquals(new int[][]{new int[]{0, 0}, new int[]{1, 1}},
+        rightSquare(new int[][]{new int[]{0, 1}, new int[]{0, 1}}));
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{13, 9, 5, 1},
+            new int[]{14, 10, 6, 2},
+            new int[]{15, 11, 7, 3},
+            new int[]{16, 12, 8, 4},
+        },
+        rightSquare(new int[][]{
+            new int[]{1, 2, 3, 4},
+            new int[]{5, 6, 7, 8},
+            new int[]{9, 10, 11, 12},
+            new int[]{13, 14, 15, 16},
+        }));
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{21, 16, 11, 6, 1},
+            new int[]{22, 17, 12, 7, 2},
+            new int[]{23, 18, 13, 8, 3},
+            new int[]{24, 19, 14, 9, 4},
+            new int[]{25, 20, 15, 10, 5},
+        },
+        rightSquare(new int[][]{
+            new int[]{1, 2, 3, 4, 5},
+            new int[]{6, 7, 8, 9, 10},
+            new int[]{11, 12, 13, 14, 15},
+            new int[]{16, 17, 18, 19, 20},
+            new int[]{21, 22, 23, 24, 25},
         }));
   }
 }
