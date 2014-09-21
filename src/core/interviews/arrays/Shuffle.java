cc7328ce17ebbdc2806d8cf19f6a4a156a79aba0
Francois Rousseau <frncs.rss@gmail.com>
2014-09-21 16:46:11 +0200

    [add] shuffle array of Strings

--- src/core/interviews/arrays/Shuffle.java
+++ src/core/interviews/arrays/Shuffle.java
@@ -25,4 +25,14 @@ public class Shuffle {
     }
     return arr;  // for ease of testing
   }
+
+  public static String[] f(String[] arr) {
+    final int n = arr.length;
+    Random rnd = new Random();
+    for(int i = n - 1; i >= 0; i--) {
+      int j = rnd.nextInt(i + 1);  // we pick a random index between 0 and i
+      Swap.f(arr, i, j);
+    }
+    return arr;  // for ease of testing
+  }
 }
