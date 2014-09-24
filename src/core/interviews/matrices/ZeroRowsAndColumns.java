8619d8b4b8cf9676c87de13b1392b0c1445d3ae6
Francois Rousseau <frncs.rss@gmail.com>
2014-09-24 22:22:28 +0200

    [add] simpler implementation

--- src/core/interviews/matrices/ZeroRowsAndColumns.java
+++ src/core/interviews/matrices/ZeroRowsAndColumns.java
@@ -1,5 +1,6 @@
 package interviews.matrices;
 
+
 /**
  * Given a matrix A, set to 0 any cell for which there is a 0 in its row or column. The operation
  * has to be in-place.
@@ -123,5 +124,75 @@ public class ZeroRowsAndColumns {
       return A[i][j] == RESERVED && A[i][j + 1] == 0;
     }
     return A[i][j] == RESERVED && A[i + 1][j] == 0 && A[i][j + 1] == 0;
+  }
+
+  /**
+   * Let A be a n by m matrix.
+   * Time complexity:  O(nm)
+   * Space complexity: O(1)
+   */
+  public static int[][] f2(int[][] A) {
+    final int n = A.length;
+    final int m = A[0].length;
+
+    // check if there is a zero in the first row? O(m)
+    boolean isThereAZeroInFirstRow = false;
+    for(int j = 0; j < m; j++) {
+      if(A[0][j] == 0) {
+        isThereAZeroInFirstRow = true;
+      }
+    }
+
+    // check if there is a zero in the first column? O(n)
+    boolean isThereAZeroInFirstColumn = false;
+    for(int i = 0; i < n; i++) {
+      if(A[i][0] == 0) {
+        isThereAZeroInFirstColumn = true;
+      }
+    }
+
+    // mark in the first row/column if the row/column should be set to zero, O(nm)
+    for(int i = 1; i < n; i++) {
+      for(int j = 1; j < m; j++) {
+        if(A[i][j] == 0) {
+          A[i][0] = 0;
+          A[0][j] = 0;
+        }
+      }
+    }
+
+    // for each 0 in the first row, set the corresponding column to 0s, O(nm)
+    for(int j = 1; j < m; j++) {
+      if(A[0][j] == 0) {
+        for(int i = 1; i < n; i++) {  // set the entire column to 0s
+          A[i][j] = 0;
+        }
+      }
+    }
+
+    // for each 0 in the first column, set the corresponding row to 0s, O(nm)
+    for(int i = 1; i < n; i++) {
+      if(A[i][0] == 0) {
+        for(int j = 1; j < m; j++) {  // set the entire row to 0s
+          A[i][j] = 0;
+        }
+      }
+    }
+
+    // if there was a zero in the first row originally, all its cells should be set to zero, O(m)
+    if(isThereAZeroInFirstRow) {
+      for(int j = 0; j < m; j++) {
+        A[0][j] = 0;
+      }
+    }
+
+    // if there was a zero in the first column originally, all its cells should be set to zero, O(n)
+    if(isThereAZeroInFirstColumn) {
+      for(int i = 0; i < n; i++) {
+        A[i][0] = 0;
+      }
+    }
+
+    return A;  // for ease of testing
   }
 }
--- src/test/interviews/matrices/ZeroRowsAndColumnsTest.java
+++ src/test/interviews/matrices/ZeroRowsAndColumnsTest.java
@@ -1,6 +1,7 @@
 package interviews.matrices;
 
 import static interviews.matrices.ZeroRowsAndColumns.f;
+import static interviews.matrices.ZeroRowsAndColumns.f2;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,7 +12,7 @@ import org.junit.Test;
  */
 public class ZeroRowsAndColumnsTest {
   @Test
-  public void test() {
+  public void test_f() {
     Assert.assertArrayEquals(new int[][]{new int[]{0}}, f(new int[][]{new int[]{0}}));
     Assert.assertArrayEquals(new int[][]{new int[]{1}}, f(new int[][]{new int[]{1}}));
     Assert.assertArrayEquals(new int[][]{new int[]{2}}, f(new int[][]{new int[]{2}}));
@@ -72,6 +73,73 @@ public class ZeroRowsAndColumnsTest {
             new int[]{0, 2, 7, 3, 4, 5}, new int[]{0, 2, 7, 8, 9, 1}
         },
         f(new int[][]{
+            new int[]{0, 2, 1, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
+            new int[]{1, 2, 7, 3, 4, 5}, new int[]{6, 2, 7, 8, 9, 1}
+        }));
+  }
+
+  @Test
+  public void test_f2() {
+    Assert.assertArrayEquals(new int[][]{new int[]{0}}, f2(new int[][]{new int[]{0}}));
+    Assert.assertArrayEquals(new int[][]{new int[]{1}}, f2(new int[][]{new int[]{1}}));
+    Assert.assertArrayEquals(new int[][]{new int[]{2}}, f2(new int[][]{new int[]{2}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{1, 2}, new int[]{3, 4}},
+        f2(new int[][]{new int[]{1, 2}, new int[]{3, 4}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{3, 0}},
+        f2(new int[][]{new int[]{1, 0}, new int[]{3, 4}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{0, 4}},
+        f2(new int[][]{new int[]{0, 1}, new int[]{3, 4}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{0, 0}},
+        f2(new int[][]{new int[]{0, 1}, new int[]{3, 0}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{new int[]{0, 0}, new int[]{0, 0}},
+        f2(new int[][]{new int[]{1, 2}, new int[]{0, 0}}));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 0, 0, 0, 0, 0},
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 0, 0, 0, 0, 0}
+        },
+        f2(new int[][]{
+            new int[]{1, 2, 0, 3, 4, 0}, new int[]{5, 6, 7, 0, 8, 9},
+            new int[]{1, 2, 0, 3, 4, 5}, new int[]{6, 0, 7, 8, 9, 1}
+        }));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{5, 0, 0, 3, 8, 9},
+            new int[]{1, 0, 0, 3, 4, 5}, new int[]{0, 0, 0, 0, 0, 0}
+        },
+        f2(new int[][]{
+            new int[]{1, 2, 0, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
+            new int[]{1, 2, 7, 3, 4, 5}, new int[]{6, 0, 7, 8, 9, 1}
+        }));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{1, 2, 0, 3, 4, 2}, new int[]{5, 6, 0, 3, 8, 9},
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{6, 2, 0, 8, 9, 1}
+        },
+        f2(new int[][]{
+            new int[]{1, 2, 1, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
+            new int[]{1, 2, 0, 3, 4, 5}, new int[]{6, 2, 7, 8, 9, 1}
+        }));
+
+    Assert.assertArrayEquals(
+        new int[][]{
+            new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 6, 7, 3, 8, 9},
+            new int[]{0, 2, 7, 3, 4, 5}, new int[]{0, 2, 7, 8, 9, 1}
+        },
+        f2(new int[][]{
             new int[]{0, 2, 1, 3, 4, 2}, new int[]{5, 6, 7, 3, 8, 9},
             new int[]{1, 2, 7, 3, 4, 5}, new int[]{6, 2, 7, 8, 9, 1}
         }));
