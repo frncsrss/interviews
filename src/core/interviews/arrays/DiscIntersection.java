789f62327382b2dff0355e6f39bee6855b239c39
Francois Rousseau <frncs.rss@gmail.com>
2014-11-02 11:43:05 +0100

    [mod] clean up

--- src/core/interviews/arrays/DiscIntersection.java
+++ src/core/interviews/arrays/DiscIntersection.java
@@ -1,23 +1,20 @@
 package interviews.arrays;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 /**
- * Given an array A of N integers, we draw N discs in a 2D plane such that the
- * i-th disc is centered on (0,i) and has a radius of A[i].
- * We say that the j-th disc and k-th disc intersect if j ­ k and j-th and k-th
- * discs have at least one common point.
+ * Given an array of n integers, we draw n discs in a 2D plane such that the i-th disc is centered
+ * on (0,i) and has a radius of radius[i].
  *
- * Write a function that, given an array A describing N discs as explained above,
- * returns the number of pairs of intersecting discs.
+ * We say that the j-th disc and k-th disc intersect if j ­ k and j-th and k-th discs have at least
+ * one common point.
  *
- * For example, given N=6 and:
- * A[0] = 1  A[1] = 5  A[2] = 2
- * A[3] = 1  A[4] = 4  A[5] = 0
- * intersecting discs appear in eleven pairs of elements:
+ * Write a function that, given an array describing N discs as explained above, returns the number
+ * of pairs of intersecting discs.
+ *
+ * For example, given n=6 and radius = {1, 5, 2, 1, 4, 0}
+ * there are 11 pairs of intersecting discs:
  * 0 and 1,
  * 0 and 2,
  * 0 and 4,
@@ -34,64 +31,16 @@ import java.util.List;
  * @author Francois Rousseau
  */
 public class DiscIntersection {
-  public static int f(int[] A) {
-    return f1(A);
-  }
 
-  public static int f1(int[] A) {
-    int count = 0, N=A.length;
-    List<Interval> list = new ArrayList<Interval>();
-    for(int i = 0; i < N; i++) {
-      list.add(new Interval(i-A[i], i+A[i]));
-    }
-    Collections.sort(list, new Comparator<Interval>() {
-      @Override
-      public int compare(Interval i1, Interval i2) {
-        if(i1.min > i2.min) {
-          return 1;
-        } else if(i1.min < i2.min) {
-          return -1;
-        }
-        return new Integer(i2.max).compareTo(i1.max);
-      }
-    });
-
-    // no need to process the last disc.
-    for(int i = 0; i < N-1; i++) {
-      // early pruning.
-      if(list.get(i+1).min > list.get(i).max) {
-        continue;
-      }
-
-      // binary search in the subarray at the right of the index.
-      int a = i+1;
-      int b = N-1;
-      int c = (a+b)/2;
-      while(a < b) {
-        if(list.get(c).min > list.get(i).max) {
-          b = c-1;
-        } else {
-          a = c+1;
-        }
-        c = (a+b)/2;
-      }
-      if(list.get(c).min > list.get(i).max) {
-        c--;
-      }
-
-      count += c-i;
-      if(count > 10000000) {
-        return -1;
-      }
-    }
-    return count;
-  }
-
-  public static int f2(int[] A) {
+  /**
+   * Time complexity:  O(n^2)
+   * Space complexity: O(1)
+   */
+  public static int f1(int[] radius) {
     int count = 0;
-    for (int i = 0; i < A.length; i++) {
-      for (int j = i+1; j < A.length; j++) {
-        if (i + A[i] >= j - A[j]) {
+    for (int i = 0; i < radius.length; i++) {
+      for (int j = i + 1; j < radius.length; j++) {
+        if (i + radius[i] >= j - radius[j]) {
           count++;
         }
       }
@@ -99,49 +48,108 @@ public class DiscIntersection {
     return count;
   }
 
-  public static int f3(int[] A) {
-    int N = A.length;
-    List<Boundary> list = new ArrayList<Boundary>();
-    for(int i = 0; i < N; i++) {
-      list.add(new Boundary(i-A[i], false));
-      list.add(new Boundary(i+A[i], true));
+  /**
+   * Time complexity:  O(nlogn)
+   * Space complexity: O(n)
+   */
+  public static int f2(int[] radius) {
+    final int n = radius.length;
+
+    // transform the array of n radius into an array of n intervals
+    Interval[] intervals = new Interval[n];
+    for(int i = 0; i < n; i++) {
+      intervals[i] = new Interval(i - radius[i], i + radius[i]);
     }
-    Collections.sort(list, new Comparator<Boundary>() {
+    // sort the intervals in ascending lower bound then ascending upper bound
+    Arrays.sort(intervals, new Comparator<Interval>() {  // O(nlogn)
       @Override
-      public int compare(Boundary b1, Boundary b2) {
-        return new Integer(b1.value).compareTo(b2.value);
+      public int compare(Interval i1, Interval i2) {
+        if(i1.lo > i2.lo) {
+          return 1;
+        } else if(i1.lo < i2.lo) {
+          return -1;
+        }
+        return new Integer(i1.hi).compareTo(i2.hi);
       }
     });
-    int count = 0, ret = 0;
-    for(int i = 0; i < 2*N; i++) {
-      if(!list.get(i).isEnd) {
-        count++;
+
+    int count = 0;
+    for(int i = 0; i < n - 1; i++) {  // O(nlogn)
+      // binary search of intervals[i].hi in intervals[i+1...n-1], O(logn)
+      final int value = intervals[i].hi;
+      int lo = i + 1;
+      int hi = n - 1;
+      while(lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if(intervals[mid].lo > value) {
+          hi = mid - 1;
+        } else {
+          lo = mid + 1;
+        }
+      }
+
+      count += hi - i;
+    }
+    return count;
+  }
+
+  private static class Interval {
+    final int lo, hi;
+
+    public Interval(int lo, int hi) {
+      this.lo = lo;
+      this.hi = hi;
+    }
+  }
+
+  /**
+   * Time complexity:  O(nlogn)
+   * Space complexity: O(n)
+   */
+  public static int f3(int[] radius) {
+    final int n = radius.length;
+
+    // transform the array of n radius into an array of 2n boundaries
+    Endpoint[] boundaries = new Endpoint[2 * n];
+    for(int i = 0; i < n; i++) {
+      boundaries[i] = new Endpoint(Endpoint.Type.START, i - radius[i]);
+      boundaries[i + n] = new Endpoint(Endpoint.Type.END, i + radius[i]);
+    }
+    // sort the boundaries
+    Arrays.sort(boundaries);
+
+    int count = 0;
+    int start = 0;
+    for(int i = 0; i < 2 * n; i++) {
+      if(Endpoint.Type.START.equals(boundaries[i].type)) {
+        start++;
       } else {
-        count--;
-        ret += count;
+        start--;
+        count += start;
       }
     }
-    return ret;
+    return count;
   }
-}
 
-class Interval {
-  int min, max;
-  public Interval(int min, int max) {
-    this.min = min;
-    this.max = max;
-  }
-  @Override
-  public String toString() {
-    return "("+min+", "+max+")";
-  }
-}
+  private static class Endpoint implements Comparable<Endpoint> {
+    public static enum Type {START, END};
 
-class Boundary {
-  int value;
-  boolean isEnd;
-  public Boundary(int value, boolean isEnd) {
-    this.value = value;
-    this.isEnd = isEnd;
+    private final Type type;  // start or end boundary
+    private final int v;
+
+    public Endpoint(Type type, int value) {
+      this.type = type;
+      this.v = value;
+    }
+
+    @Override
+    public int compareTo(Endpoint that) {
+      if(this.v < that.v) return -1;
+      if(this.v > that.v) return +1;
+      // switch sign if exclusive ending range endpoint
+      if(Type.START.equals(this.type) && Type.END.equals(that.type)) return -1;
+      if(Type.END.equals(this.type) && Type.START.equals(that.type)) return +1;
+      return 0;
+    }
   }
 }
