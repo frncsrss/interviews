81c20832ed7ca39697e0a052b8de4ada22cda69a
Francois Rousseau <frncs.rss@gmail.com>
2015-03-08 19:33:58 +0100

    [add] binary search in rotated sorted array

--- src/core/interviews/arrays/BinarySearch.java
+++ src/core/interviews/arrays/BinarySearch.java
@@ -28,4 +28,57 @@ public class BinarySearch {
     }
     return -lo - 1;  // key not found (-1 to avoid the 0 ambiguity)
   }
+
+  /**
+   * Binary search in a sorted array in increasing order but rotated by some arbitrary value.
+   *
+   * Let n = length(arr).
+   * Time complexity:  O(logn)
+   * Space complexity: O(1)
+   */
+  public static int f2(int[] arr, int x) {
+    int lo = 0;
+    int hi = arr.length - 1;
+    while(arr[lo] >= arr[hi] && lo < hi) {  // find the minimum element in the array
+      int mid = lo + hi >>> 1;
+      if(arr[mid] >= arr[hi]) {
+        lo = mid + 1;
+      } else {
+        hi = mid;
+      }
+    }
+
+    int pivot = lo;  // index of the minimum element in the array
+    if(x < arr[pivot]) {
+      return -pivot - 1;
+    }
+    if(pivot > 0 && x > arr[pivot - 1]) {
+      return -pivot - 2;
+    }
+    if(pivot == 0 && x > arr[arr.length - 1]) {
+      return -arr.length - 2;
+    }
+
+    if(x <= arr[arr.length - 1]) {
+      return f(arr, x, pivot, arr.length - 1);
+    }
+    return f(arr, x, 0, pivot - 1);
+  }
+
+  /**
+   * Recursive binary search.
+   */
+  private static int f(int[] arr, int x, int lo, int hi) {
+    if(lo > hi) {
+      return -lo - 1;
+    }
+    int mid = lo + hi >>> 1;  // prevent possible overflow
+    if(arr[mid] < x) {
+      return f(arr, x, mid + 1, hi);
+    } else if(arr[mid] > x) {
+      return f(arr, x, lo, mid - 1);
+    } else {
+      return mid;  // key found
+    }
+  }
 }
--- src/test/interviews/arrays/BinarySearchTest.java
+++ src/test/interviews/arrays/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package interviews.arrays;
 
 import static interviews.arrays.BinarySearch.f;
+import static interviews.arrays.BinarySearch.f2;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,7 +12,7 @@ import org.junit.Test;
  */
 public class BinarySearchTest {
   @Test
-  public void test() {
+  public void test_f() {
     Assert.assertEquals(4,
         f(new int[]{6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97}, 33));
     Assert.assertEquals(5,
@@ -22,5 +23,19 @@ public class BinarySearchTest {
     Assert.assertEquals(2, -f(new int[] {1, 1}, 2) - 1);
     Assert.assertEquals(1, f(new int[] {1, 1, 2, 3}, 1));
     Assert.assertEquals(2, f(new int[] {1, 1, 2, 3}, 2));
+  }
+
+  @Test
+  public void test_f2() {
+    Assert.assertEquals(4,
+        f2(new int[]{6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97}, 33));
+    Assert.assertEquals(8,
+        f2(new int[]{93, 95, 96, 97, 6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84}, 33));
+    Assert.assertEquals(13,
+        f2(new int[]{51, 53, 64, 72, 84, 93, 95, 96, 97, 6, 13, 14, 25, 33, 43}, 33));
+    Assert.assertEquals(1, -f2(new int[] {1, 3}, 2) - 1);
+    Assert.assertEquals(2, -f2(new int[] {1, 1}, 2) - 1);
+    Assert.assertEquals(1, f2(new int[] {1, 1, 2, 3}, 1));
+    Assert.assertEquals(2, f2(new int[] {1, 1, 2, 3}, 2));
   }
 }
