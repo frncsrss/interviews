5781f2fdafe1d0e87b2df44c70a9038eaf7e13f0
Francois Rousseau <frncs.rss@gmail.com>
2015-07-19 13:38:00 +0200

    [add] memoization for quadratic complexity

--- src/core/interviews/strings/Splitter.java
+++ src/core/interviews/strings/Splitter.java
@@ -2,6 +2,9 @@ package interviews.strings;
 
 import interviews.trees.Trie;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Split a string with missing whitespace if possible. Highlight EXLs (words not in dictionary).
  * A dictionary of valid words is given as input.
@@ -12,25 +15,33 @@ public class Splitter {
   private static final String SEPARATOR = " ";
 
   public static String f(String s, Trie dict) {
-    return f(s, dict, 0, 1).s;
+    return f(s, dict, new HashMap<Integer, Result>(), 0, 1).s;
   }
 
   /**
    * Do we put a whitespace after the word s[start, end) or do we include another character,
    * i.e. do we break or do we continue?
+   *
+   * a memoy is a form of cache made for memoization, drops the complexity in time from O(2^n) to
+   * O(n^2).
    */
-  private static Result f(String s, Trie dict, int start, int end) {
+  private static Result f(String s, Trie dict, Map<Integer, Result> memoy, int start, int end) {
     if(end > s.length()) {
       String current = s.substring(start);
       return new Result(current.toUpperCase(), current.length(), true);
     }
 
+    if(memoy.containsKey(start)) {
+      return memoy.get(start).clone();
+    }
+
     String current = s.substring(start, end);
 
     // what if we put a whitespace after the current word
-    Result withBreak = f(s, dict, end, end + 1);
+    Result withBreak = f(s, dict, memoy, end, end + 1);
     if(!dict.contains(current)) {  // if the current word is not a valid prefix, let's bail out
       withBreak.prependWithEXL(current.toUpperCase());
+      memoy.put(start, withBreak.clone());
       return withBreak;
     } else if (dict.isValid(current)) {
       withBreak.prependWithWord(current);
@@ -39,10 +50,12 @@ public class Splitter {
     }
 
     // what if we consider another character in the current word
-    Result withContinue = f(s, dict, start, end + 1);
+    Result withContinue = f(s, dict, memoy, start, end + 1);
 
     // we favor continuation over break (e.g., 'awesome' over 'a we some')
-    return Result.min(withContinue, withBreak);
+    Result min = Result.min(withContinue, withBreak);
+    memoy.put(start, min.clone());
+    return min;
   }
 
   private static class Result {
@@ -54,6 +67,11 @@ public class Splitter {
       this.s = s;
       this.exl = exl;
       this.startsWithEXL = startsWithEXL;
+    }
+
+    @Override
+    public Result clone() {
+      return new Result(s, exl, startsWithEXL);
     }
 
     public void prependWithEXL(String s) {
